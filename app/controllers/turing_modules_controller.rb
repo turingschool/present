675dfc08cb6e8cbca932be75d552105a7ae0be14
Meg Stang <mstang15@gmail.com>
2021-09-22 15:30:56 -0600

    Add create action for modules

--- app/controllers/turing_modules_controller.rb
+++ app/controllers/turing_modules_controller.rb
@@ -3,4 +3,18 @@ class TuringModulesController < ApplicationController
     @module = TuringModule.find(params[:id])
     @attendances = @module.attendances.order(created_at: :desc)
   end
+
+  def create
+    inning = Inning.find(params[:inning_id])
+    new_module = inning.turing_modules.create(turing_module_params)
+    redirect_to inning_path(inning)
+  end 
+
+  private
+
+  def turing_module_params
+    params[:google_spreadsheet_id] = params[:google_spreadsheet_link].split('/')[5]
+    # binding.pry
+    params.permit(:name, :google_spreadsheet_id, :google_sheet_name)
+  end 
 end
