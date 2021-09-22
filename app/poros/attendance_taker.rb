fef1a997371d8d066402f0c4c20aef4a931525c9
Brian Zanti <brianzanti@gmail.com>
2021-09-22 15:45:41 -0500

    completes attendance taking iteration 1

--- app/poros/attendance_taker.rb
+++ app/poros/attendance_taker.rb
@@ -4,12 +4,18 @@ class AttendanceTaker
     sheet_matrix = sheet_data[:values]
     zoom_names = sheet_matrix[1]
     # MegsZoomClass.whatever(zoom_names, attendance.zoom_meeting_id)
-
-
+    participant_report = ZoomFacade.past_participants_in_meeting(attendance.zoom_meeting_id, zoom_names)
+    if participant_report
+      # set a background worker to start 5 minutes after the meeting end time
+    end
     datetime = DateTime.parse('2021-09-21T15:00:00Z')
     am_or_pm = am_or_pm(datetime)
     column_index = find_column_index(sheet_matrix, datetime)
-    binding.pry
+    zoom_names.each_with_index do |name, row_index|
+      next if row_index == 0
+      sheet_matrix[column_index][row_index] = participant_report[:participants][name].status
+    end
+    response = GoogleSheetsService.update_sheet(attendance.turing_module, user, sheet_matrix)
   end
 
   def self.am_or_pm(datetime)
