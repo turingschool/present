c3fe7b8894d8c4fafa86618d19ba5b676fcf1508
Brian Zanti <brianzanti@gmail.com>
2021-09-22 12:00:21 -0500

    adds relationships for innings and modules

--- app/models/turing_module.rb
+++ app/models/turing_module.rb
@@ -1,5 +1,6 @@
 class TuringModule < ApplicationRecord
   belongs_to :inning
+  has_many :attendances
 
   validates_presence_of :name, :google_spreadsheet_id, :google_sheet_name
 end
--- spec/models/turing_module_spec.rb
+++ spec/models/turing_module_spec.rb
@@ -5,4 +5,5 @@ RSpec.describe TuringModule, type: :model do
   it {should validate_presence_of :google_spreadsheet_id}
   it {should validate_presence_of :google_sheet_name}
   it {should belong_to :inning}
+  it {should have_many :attendances}
 end
