b650d3fbf956b29289dd45abb70f747501d925fc
Brian Zanti <brianzanti@gmail.com>
2021-09-22 12:49:34 -0500

    adds google oauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'welcome#index'
+  get '/auth/google_oauth2/callback', to: 'sessions#create'
 end
