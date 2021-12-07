d79b10d34f28464f677d8cac492441a11fdf55e3
Brian Zanti <brianzanti@gmail.com>
2021-12-07 11:59:10 -0600

    changes root to welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'user/innings#index'
+  root to: 'welcome#index'
   get '/welcome', to: 'welcome#index'
   get '/auth/google_oauth2/callback', to: 'sessions#create'
   delete '/sessions', to: 'sessions#destroy'
