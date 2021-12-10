21fc51dcef62a04454f236cf2aeaadd77da6e7be
Brian Zanti <brianzanti@gmail.com>
2021-12-10 16:09:34 -0600

    adds skeleton for modules show page spec

---
+++ spec/features/modules/show_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe 'Modules show page' do
+  it 'shows the modules attriutes' do
+  end
+
+#   As a logged in user,
+# when I visit a Module's show page,
+# then I see a heading with the module's name (ex. FE Mod 3)
+# and I see the name of the inning listed below the heading
+# and I see a link to the Google Sheet that tracks attendance for this mod
+# and I see whether or not Calendar Integration is active for this mod.
+end
