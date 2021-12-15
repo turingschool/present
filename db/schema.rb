1812cf134847fda0e0482cb7ecef571de42aeb47
Brian Zanti <brianzanti@gmail.com>
2021-12-14 18:35:44 -0600

    adds past attendances to module show page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_10_222506) do
+ActiveRecord::Schema.define(version: 2021_12_14_224634) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2021_12_10_222506) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.string "meeting_title"
+    t.datetime "meeting_time"
     t.index ["turing_module_id"], name: "index_attendances_on_turing_module_id"
     t.index ["user_id"], name: "index_attendances_on_user_id"
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,18 @@
+@user = User.first
+
+Attendance.destroy_all
+TuringModule.destroy_all
+Inning.destroy_all
+
 @inning = Inning.create!(name: "2108")
-@turing_module = @inning.turing_modules.create!(program: 'Combined', module_number: 4, google_spreadsheet_id: '1v3C4DXVmmvV1r7vEuo0Xo58AwAJ-SligrplOiLUjWnw')
-@turing_module = @inning.turing_modules.create!(program: 'FE', module_number: 1,google_spreadsheet_id: '10MNyOoYB4w9QCj9y_90DHVUe8DX2OO8ZoGUm8_4suf4', google_sheet_name: "2105")
-@turing_module = @inning.turing_modules.create!(program: 'FE', module_number: 2,google_spreadsheet_id: '10MNyOoYB4w9QCj9y_90DHVUe8DX2OO8ZoGUm8_4suf4', google_sheet_name: "2107")
-@turing_module = @inning.turing_modules.create!(program: 'FE', module_number: 3,google_spreadsheet_id: '10MNyOoYB4w9QCj9y_90DHVUe8DX2OO8ZoGUm8_4suf4', google_sheet_name: "2108")
-@turing_module = @inning.turing_modules.create!(program: 'BE', module_number: 1,google_spreadsheet_id: '13jZKWzDx87Epgf_DBOy2k2ilFlJXfJU3MdQ8tqv9HaM', google_sheet_name: "2105")
-@turing_module = @inning.turing_modules.create!(program: 'BE', module_number: 2,google_spreadsheet_id: '13jZKWzDx87Epgf_DBOy2k2ilFlJXfJU3MdQ8tqv9HaM', google_sheet_name: "2107")
-@turing_module = @inning.turing_modules.create!(program: 'BE', module_number: 3,google_spreadsheet_id: '13jZKWzDx87Epgf_DBOy2k2ilFlJXfJU3MdQ8tqv9HaM', google_sheet_name: "2108")
+@mod4 = @inning.turing_modules.create!(program: 'Combined', module_number: 4, google_spreadsheet_id: '1v3C4DXVmmvV1r7vEuo0Xo58AwAJ-SligrplOiLUjWnw')
+@fe1 = @inning.turing_modules.create!(program: 'FE', module_number: 1,google_spreadsheet_id: '10MNyOoYB4w9QCj9y_90DHVUe8DX2OO8ZoGUm8_4suf4', google_sheet_name: "2105")
+@fe2 = @inning.turing_modules.create!(program: 'FE', module_number: 2,google_spreadsheet_id: '10MNyOoYB4w9QCj9y_90DHVUe8DX2OO8ZoGUm8_4suf4', google_sheet_name: "2107")
+@fe3 = @inning.turing_modules.create!(program: 'FE', module_number: 3,google_spreadsheet_id: '10MNyOoYB4w9QCj9y_90DHVUe8DX2OO8ZoGUm8_4suf4', google_sheet_name: "2108")
+@be1 = @inning.turing_modules.create!(program: 'BE', module_number: 1,google_spreadsheet_id: '13jZKWzDx87Epgf_DBOy2k2ilFlJXfJU3MdQ8tqv9HaM', google_sheet_name: "2105")
+@be2 = @inning.turing_modules.create!(program: 'BE', module_number: 2,google_spreadsheet_id: '13jZKWzDx87Epgf_DBOy2k2ilFlJXfJU3MdQ8tqv9HaM', google_sheet_name: "2107")
+@be3 = @inning.turing_modules.create!(program: 'BE', module_number: 3,google_spreadsheet_id: '13jZKWzDx87Epgf_DBOy2k2ilFlJXfJU3MdQ8tqv9HaM', google_sheet_name: "2108")
+
+@fe3.attendances.create!(zoom_meeting_id: 95490216907, meeting_title: 'Cohort Standup', meeting_time: "2021-12-14 16:00:00 UTC", user: @user)
+@fe3.attendances.create!(zoom_meeting_id: 94426586781, meeting_title: 'PD: Alumni Panel', meeting_time: "2021-12-9 20:00:00 UTC", user: @user)
+@fe3.attendances.create!(zoom_meeting_id: 99157739086, meeting_title: 'M4 Gear Up', meeting_time: "2021-12-7 20:00:00 UTC", user: @user)
