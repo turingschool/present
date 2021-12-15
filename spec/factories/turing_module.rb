5f95c5d07751066d04ea11fa2612f7e96ff8939e
Brian Zanti <brianzanti@gmail.com>
2021-12-15 11:44:02 -0600

    udpates turing_module to associate to sheet/spreadsheet models

--- spec/factories/turing_module.rb
+++ spec/factories/turing_module.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
   factory :turing_module do
-    google_spreadsheet_id {'lfkj34h79ytojis498'}
     program { :FE }
     module_number { 3 }
     inning
--- spec/models/turing_module_spec.rb
+++ spec/models/turing_module_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe TuringModule, type: :model do
-  it { should validate_presence_of :google_spreadsheet_id }
   it { should validate_presence_of :program }
   it { should validate_numericality_of(:module_number).is_greater_than_or_equal_to(1) }
   it { should validate_numericality_of(:module_number).is_less_than_or_equal_to(4) }
@@ -10,13 +9,12 @@ RSpec.describe TuringModule, type: :model do
   it { should validate_inclusion_of(:calendar_integration).in_array([true, false]) }
   it { should belong_to :inning }
   it { should have_many :attendances }
-  xit {should validate_presence_of :google_sheet_name} # Do we need this?
+  it { should have_one :google_sheet}
 
   describe 'instance methods' do
     describe '#name' do
       it 'returns a combo of the module number and program' do
-        inning = Inning.create!(name: '2104')
-        test_module = inning.turing_modules.create!(google_spreadsheet_id: 'sdlfj', program: :FE, module_number: 3)
+        test_module = create(:turing_module, program: :FE, module_number: 3)
         expect(test_module.name).to eq('FE Mod 3')
       end
     end
