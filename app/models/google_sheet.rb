ac6c8784f5fa380d5a0bb0a501ffc043df0ad95c
Brian Zanti <brianzanti@gmail.com>
2021-12-15 15:57:19 -0600

    adds link method to create an external link with sheet/spreadsheet data

--- app/models/google_sheet.rb
+++ app/models/google_sheet.rb
@@ -3,4 +3,8 @@ class GoogleSheet < ApplicationRecord
   belongs_to :google_spreadsheet
 
   validates_presence_of :google_id, :name
+
+  def link
+    "https://docs.google.com/spreadsheets/d/#{self.google_spreadsheet.google_id}#gid=#{self.google_id}"
+  end
 end
--- spec/models/google_sheet_spec.rb
+++ spec/models/google_sheet_spec.rb
@@ -5,4 +5,15 @@ RSpec.describe GoogleSheet do
   it {should validate_presence_of :google_id}
   it {should belong_to :google_spreadsheet}
   it {should belong_to :turing_module}
+
+  describe 'instance methods' do
+    describe '#link' do
+      it 'fails' do
+        spreadsheet = GoogleSpreadsheet.create!(google_id: '1sb75ubr7sTEwB20LdvA940yky9jPdcRq_MvG-zBvSLY')
+        turing_module = create(:turing_module)
+        sheet = spreadsheet.google_sheets.create!(google_id: '1626710953', name: '2110', turing_module: turing_module)
+        expect(sheet.link).to eq('https://docs.google.com/spreadsheets/d/1sb75ubr7sTEwB20LdvA940yky9jPdcRq_MvG-zBvSLY#gid=1626710953')
+      end
+    end
+  end
 end
