9acb4336e239b5ed66e7b29869b346abe7f85858
Brian Zanti <brianzanti@gmail.com>
2021-12-15 15:57:45 -0600

    adds google sheet and spreadsheet factories

---
+++ spec/factories/google_sheet.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :google_sheet do
+    google_id { '1626710953' }
+    name { '2110' }
+    google_spreadsheet
+    turing_module
+  end
+end
---
+++ spec/factories/google_spreadsheet.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :google_spreadsheet do
+    google_id { '1sb75ubr7sTEwB20LdvA940yky9jPdcRq_MvG-zBvSLY' }
+  end
+end
