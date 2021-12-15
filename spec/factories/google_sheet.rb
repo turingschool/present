be6078ba681a6d3e781f8225e4fadc16ae853c70
Brian Zanti <brianzanti@gmail.com>
2021-12-15 16:25:15 -0600

    adds factories for use with test spreadsheets

--- spec/factories/google_sheet.rb
+++ spec/factories/google_sheet.rb
@@ -1,8 +1,15 @@
 FactoryBot.define do
   factory :google_sheet do
-    google_id { '1626710953' }
-    name { '2110' }
+    google_id { '<google_sheet_id>' }
+    name { '<google_sheet_name>' }
     google_spreadsheet
     turing_module
+
+    factory :fe1_attendance_sheet do
+      google_id { '249481521' }
+      name { '2111' }
+      google_spreadsheet factory: :fe_attendance
+      turing_module factory: :fe1
+    end
   end
 end
--- spec/factories/turing_module.rb
+++ spec/factories/turing_module.rb
@@ -3,5 +3,13 @@ FactoryBot.define do
     program { :FE }
     module_number { 3 }
     inning
+
+    factory :frontend do
+      program { :FE }
+
+      factory :fe1 do
+        module_number { 1 }
+      end
+    end
   end
 end
