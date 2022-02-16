45d1696a4dedc104937576c3dd77d54aa445f0dd
Brian Zanti <brianzanti@gmail.com>
2022-02-16 13:59:32 -0600

    removes validations for students

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,5 +3,5 @@ class Student < ApplicationRecord
   has_many :student_attendances
   has_many :attendances, through: :student_attendances
 
-  validates_presence_of :zoom_id, :zoom_email, :name
+  # validates_presence_of :zoom_id, :zoom_email, :name
 end
