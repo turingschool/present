8637fa42d4e29a06e206336b7918e3674a083568
Brian Zanti <brianzanti@gmail.com>
2022-02-16 17:31:53 -0600

    adds enum for student attendance status

--- spec/models/student_attendance_spec.rb
+++ spec/models/student_attendance_spec.rb
@@ -5,4 +5,6 @@ RSpec.describe StudentAttendance, type: :model do
     it {should belong_to :student}
     it {should belong_to :attendance}
   end
+
+  it {should define_enum_for(:status).with_values(present: 0, tardy: 1, absent: 2)}
 end
