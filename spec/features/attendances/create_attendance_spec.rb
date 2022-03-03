4e7536fae11e5aebdb08ac720bdcd865024e8736
Brian Zanti <brianzanti@gmail.com>
2022-03-03 14:47:21 -0600

    adds test student objects and expectations

--- spec/features/attendances/create_attendance_spec.rb
+++ spec/features/attendances/create_attendance_spec.rb
@@ -80,9 +80,9 @@ RSpec.describe 'Creating an Attendance' do
     fill_in :attendance_zoom_meeting_id, with: test_zoom_meeting_id
     click_button 'Submit'
 
-    click_link('23 Students')
+    click_link("#{expected_students.length} Students")
 
-    expect(page).to have_css('.student', count: 23)
+    expect(page).to have_css('.student', count: expected_students.length)
     expected_students.each do |student|
       expect(page).to have_content(student.name)
       expect(page).to have_content(student.zoom_email)
@@ -91,20 +91,6 @@ RSpec.describe 'Creating an Attendance' do
   end
 
   it 'creates students attendances'
-
-# As a logged in user
-# When I visit the show page for a module with no students,
-# and I click the button to "Add an Attendance",
-# Then I am redirected to the new attendance page
-# where I see a check box with a label
-# "Use this meeting to add students to your module".
-# When I click this checkbox,
-# And I add a Zoom meeting id to the text field,
-# and I click the submit button,
-# Then I am redirected back to the module's show page,
-# And I now see a link for '23 Students' (or however many students were in the Zoom meeting)
-# And when I click this link I am taken to the module's student index
-# where I see the information for each student that was on the Zoom call.
 
   let(:expected_attendance_values){
     [
@@ -154,6 +140,50 @@ RSpec.describe 'Creating an Attendance' do
   }
 
   let(:expected_students){
-    Student.new(name: 'test', zoom_email: 'test', zoom_id: 'test')
+    [
+      Student.new(zoom_id: "16778240", name: "Ryan Teske (He/Him)", zoom_email: "ryanteske@outlook.com"),
+      Student.new(zoom_id: "16779264", name: "Isika P (she/her# BE)", zoom_email: ""),
+      Student.new(zoom_id: "16780288", name: "Natalia ZV (she/her)# FE", zoom_email: "nzamboniv@gmail.com"),
+      Student.new(zoom_id: "16781312", name: "Jamie P (she/her)# BE", zoom_email: "jamiejpace@gmail.com"),
+      Student.new(zoom_id: "16782336", name: "Tanner D (he/him)# BE", zoom_email: ""),
+      Student.new(zoom_id: "16783360", name: "Kevin (he/him)", zoom_email: ""),
+      Student.new(zoom_id: "16784384", name: "Weston E# (He/Him)# BE", zoom_email: "ellisweston112@gmail.com"),
+      Student.new(zoom_id: "16785408", name: "Carlos G { he: him } FE", zoom_email: "carlosalbertogomez108@gmail.com"),
+      Student.new(zoom_id: "16786432", name: "Anna J (she/her)# FE", zoom_email: "ae.johnson2931@gmail.com"),
+      Student.new(zoom_id: "16787456", name: "Robbie Jaeger (he/him)", zoom_email: "robbie@turing.io"),
+      Student.new(zoom_id: "16788480", name: "Erin Q (she/her)# BE", zoom_email: "equinn125@gmail.com"),
+      Student.new(zoom_id: "16789504", name: "Henry S (he/him)# BE", zoom_email: "henry.schmid1@gmail.com"),
+      Student.new(zoom_id: "16790528", name: "Ozzie O (he# him) BE", zoom_email: "mikeosmonson@gmail.com"),
+      Student.new(zoom_id: "16791552", name: "Nadia N (she/her)", zoom_email: ""),
+      Student.new(zoom_id: "16792576", name: "☘️Nolan C.", zoom_email: "nolancaine2@gmail.com"),
+      Student.new(zoom_id: "16793600", name: "Brian Zanti (he/him)", zoom_email: "brian@turing.io"),
+      Student.new(zoom_id: "16794624", name: "Eric S (he/him)# FE", zoom_email: ""),
+      Student.new(zoom_id: "16795648", name: "Joshua H (he/him)# FE", zoom_email: ""),
+      Student.new(zoom_id: "16796672", name: "Renee S-Z (She/Her)# FE", zoom_email: ""),
+      Student.new(zoom_id: "16797696", name: "Ryan F (he/him)", zoom_email: "ryan@turing.io"),
+      Student.new(zoom_id: "16798720", name: "kevinn", zoom_email: ""),
+      Student.new(zoom_id: "16799744", name: "Logan V (he/him)", zoom_email: ""),
+      Student.new(zoom_id: "16800768", name: "Ezze (He/Him)# BE", zoom_email: "ezzowafai@gmail.com"),
+      Student.new(zoom_id: "16801792", name: "Khoi N (he/him) BE", zoom_email: "khoinguyen311@gmail.com"),
+      Student.new(zoom_id: "16802816", name: "Travis Rollins (he/him)", zoom_email: "travis@turing.io"),
+      Student.new(zoom_id: "16803840", name: "Dane Brophy (he/they)# BE", zoom_email: "dbrophy720@gmail.com"),
+      Student.new(zoom_id: "16804864", name: "Paul C", zoom_email: ""),
+      Student.new(zoom_id: "16805888", name: "Kelsey T (she/her# BE)", zoom_email: "kelsthompson2@gmail.com"),
+      Student.new(zoom_id: "16806912", name: "Anthony I# FE", zoom_email: "anthony.iacono@protonmail.com"),
+      Student.new(zoom_id: "16807936", name: "Nate Sheridan (he/him)", zoom_email: "nbs@dr.com"),
+      Student.new(zoom_id: "16808960", name: "Raquel H (she/her) FE", zoom_email: ""),
+      Student.new(zoom_id: "16809984", name: "Ida O (she/her)# BE", zoom_email: ""),
+      Student.new(zoom_id: "16811008", name: "Sami Peterson", zoom_email: "sami.peterson14@gmail.com"),
+      Student.new(zoom_id: "16812032", name: "Jacq W. (they/them)", zoom_email: ""),
+      Student.new(zoom_id: "16813056", name: "Erika K. (she/her) BE", zoom_email: "erika.kischuk@gmail.com"),
+      Student.new(zoom_id: "16814080", name: "Jes Jones (she/her) BE", zoom_email: ""),
+      Student.new(zoom_id: "16815104", name: "Rowan (they/them)", zoom_email: "rowanwinzer@gmail.com"),
+      Student.new(zoom_id: "16816128", name: "Phil T (he/him)# FE", zoom_email: ""),
+      Student.new(zoom_id: "16817152", name: "Sarah Rudy (she/her)# FE", zoom_email: "sarahrudy@gmail.com"),
+      Student.new(zoom_id: "16818176", name: "Bei Z (she/her) FE", zoom_email: "241zxy@gmail.com"),
+      Student.new(zoom_id: "16819200", name: "Logan V. (he/him) FE", zoom_email: "ldvsimp@gmail.com"),
+      Student.new(zoom_id: "16820224", name: "Laura C (she/her)# BE", zoom_email: "laura.mcourt@gmail.com"),
+      Student.new(zoom_id: "16821248", name: "Raquel Hill", zoom_email: "")
+    ]
   }
 end
