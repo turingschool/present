0d71c8421459c01d27fdb023950e90884f6cde4f
Brian Zanti <brianzanti@gmail.com>
2022-03-03 14:50:34 -0600

    user can click checkbox to populate mod with students

--- app/facades/create_attendance_facade.rb
+++ app/facades/create_attendance_facade.rb
@@ -6,18 +6,11 @@ class CreateAttendanceFacade
       # Use participant report to create students
     meeting_details = ZoomService.meeting_details(attendance.zoom_meeting_id)
     participants = ZoomService.past_participants_meeting_report(attendance.zoom_meeting_id)[:participants]
-    if populate_students
-      participants.each do |participant|
-        #create a student
-      end
-    end
+    attendance.turing_module.create_students_from_participants(participants) if populate_students
     # Use meeting details, participant report,
     # and module's students to create attendance values
     # Update the Google Sheet
-    ZoomFacade.past_participants_in_meeting
+    # ZoomFacade.past_participants_in_meeting
     AttendanceTaker.take_attendance(attendance, user)
-
-
-
   end
 end
