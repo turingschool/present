b18f7a9948607f37d3c58c2c168d4c05916135d8
Brian Zanti <brianzanti@gmail.com>
2022-03-29 16:02:05 -0500

    adds zoom id validations to student

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,5 +3,6 @@ class Student < ApplicationRecord
   has_many :student_attendances, dependent: :destroy
   has_many :attendances, through: :student_attendances
 
-  # validates_presence_of :zoom_id, :zoom_email, :name
+  validates_presence_of :zoom_id #, :zoom_email, :name
+  validates_uniqueness_of :zoom_id
 end
