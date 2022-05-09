8228ef6b827b9e89a83de7e8416cdbf8ceeb7bd3
Meg Stang <mstang15@gmail.com>
2022-05-09 11:36:01 -0600

    Add gem for testing html tables

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,6 +10,7 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 require 'support/factory_bot'
 require 'support/helper_methods'
+require "capybara_table/rspec"
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
