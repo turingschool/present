3f1601c18ea2bcc27bfcc8e0c7aa2e9908ce64f5
Brian Zanti <brianzanti@gmail.com>
2022-06-03 14:45:27 -0500

    adds links to current inning and dashboard

--- spec/features/nav_spec.rb
+++ spec/features/nav_spec.rb
@@ -10,10 +10,33 @@ RSpec.describe "Nav Bar" do
 
     expect(current_path).to eq(innings_path)
   end
+
   it 'non registered user cant see link to see all innings' do
     visit root_path
 
     expect(page).to_not have_link("All Innings")
+  end
+
+  it 'has a link to the dashboard' do
+     mock_login
+
+     visit root_path
+     expect(page).to have_link('Dashboard', href: '/')
+  end
+
+  it 'has a link to the current inning' do
+    mock_login
+    current_inning = create(:inning, current: true)
+
+    visit root_path
+    expect(page).to have_link('Current Inning', href: inning_path(current_inning))
+  end
+
+  it 'does not have a link to the current inning when there is no current inning' do
+    mock_login
+
+    visit root_path
+    expect(page).to_not have_link('Current Inning')
   end
 
 end
