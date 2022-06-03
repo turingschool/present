acc4a566eb62efb4f843907d35b7ccf0baeb4def
Brian Zanti <brianzanti@gmail.com>
2022-06-03 16:47:55 -0500

    adds valid_google_user? method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,5 +21,22 @@ RSpec.describe User, type: :model do
         expect(user.my_module).to eq(nil)
       end
     end
+
+    describe 'valid_google_user?' do
+      it 'returns true if the the users org is turing' do
+        user = create(:user, organization_domain: 'turing.edu')
+        expect(user.valid_google_user?).to eq(true)
+      end
+
+      it 'returns false if the users org is not turing' do
+        user = create(:user, organization_domain: 'notturing.edu')
+        expect(user.valid_google_user?).to eq(false)
+      end
+
+      it 'return false if the user has no org' do
+        user = create(:user)
+        expect(user.valid_google_user?).to eq(false)
+      end
+    end
   end
 end
