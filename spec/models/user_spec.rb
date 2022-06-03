b839413f683df98ea10ec5919b6fd318963071ec
Brian Zanti <brianzanti@gmail.com>
2022-06-03 16:24:57 -0500

    adds test for my_module method and turing_module relationship

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it {should validate_presence_of :email}
-  it {should validate_presence_of :google_oauth_token}
-  it {should validate_presence_of :google_id}
+  describe 'validations' do
+    it {should validate_presence_of :email}
+    it {should validate_presence_of :google_oauth_token}
+    it {should validate_presence_of :google_id}
+    it {should belong_to(:turing_module).optional}
+  end
+
+  describe 'instance methods' do
+    describe 'my_module' do
+      it 'returns the users module' do
+        mod = create(:turing_module)
+        user = create(:user, turing_module: mod)
+        expect(user.my_module).to eq(mod)
+      end
+
+      it 'returns nil if the user has no module' do
+        user = create(:user)
+        expect(user.my_module).to eq(nil)
+      end
+    end
+  end
 end
