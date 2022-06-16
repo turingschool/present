4c1cbd95612db3ed408b77586501a869c35da176
Meg Stang <mstang15@gmail.com>
2022-06-16 15:07:41 -0600

    Add logic to find new participants and attatch their status to the report

--- app/facades/create_attendance_facade.rb
+++ app/facades/create_attendance_facade.rb
@@ -9,13 +9,16 @@ class CreateAttendanceFacade
     attendance.update(meeting_title: meeting_title)
 
     attendance.turing_module.students.each do |student|
-      participant = participants.find {|participant| participant[:id] == student.zoom_id}
+      participants_with_matching_zoom_id = participants.find_all {|participant| participant[:id] == student.zoom_id}
+      participant = participants_with_matching_zoom_id.first
       participant_join_time = participant ? Time.parse(participant[:join_time]) : nil
       status = convert_status(participant_join_time, Time.parse(meeting_start_time))
       StudentAttendance.create(status: status, join_time: participant_join_time, attendance: attendance, student: student)
+      participants -= participants_with_matching_zoom_id
     end
 
-    # AttendanceTaker.take_attendance(attendance, user)
+    participants_with_join_time_status = attatch_status_to_participants(participants,meeting_start_time)
+    attendance.create_visiting_students(participants_with_join_time_status)
   end
 
   def self.convert_status(join_time, meeting_start_time)
@@ -25,4 +28,11 @@ class CreateAttendanceFacade
     return 'tardy' if 1 <= minutes_passed_start_time
     return 'present'
   end
+  
+  def self.attatch_status_to_participants(participants, meeting_start_time)
+    participants.map do |participant|
+      participant[:status] = convert_status(Time.parse(participant[:join_time]), Time.parse(meeting_start_time))
+      participant
+    end 
+  end 
 end
