7a93ad54ee04cdc3b3bb89af2aaa8e1965d91643
Brian Zanti <brianzanti@gmail.com>
2022-06-21 15:35:47 -0400

    refactors zoom service to use Faraday

--- app/services/zoom_service.rb
+++ app/services/zoom_service.rb
@@ -1,38 +1,34 @@
-require 'uri'
-require 'net/http'
-
 class ZoomService
-    def self.meeting_details(meeting_id)
-        zoom_api("/v2/meetings/#{meeting_id}")
-    end
+  def self.meeting_details(meeting_id)
+    response = conn.get "/v2/meetings/#{meeting_id}"
+    parse_response(response)
+  end
 
-    def self.past_participants_meeting_report(meeting_id)
-        zoom_api("/v2/report/meetings/#{meeting_id}/participants?page_size=300")
-    end
+  def self.participant_report(meeting_id)
+    response = conn.get "/v2/report/meetings/#{meeting_id}/participants?page_size=300"
+    parse_response(response)
+  end
 
 private
-    def self.zoom_api(endpoint)
-        url_string = "https://api.zoom.us" + endpoint
-        url = URI(url_string)
-
-        http = Net::HTTP.new(url.host, url.port)
-        http.use_ssl = true
-        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-        request = Net::HTTP::Get.new(url)
-        request["authorization"] = "Bearer #{generate_jwt}"
-        request["content-type"] = 'application/json'
-
-        response = http.request(request)
-        parsed_body = JSON.parse(response.body, symbolize_names: true)
-    end
-
-    def self.generate_jwt
-      payload = {
-        "iss": ENV['ZOOM_API_KEY'],
-        "exp": Time.now.to_i + 5
+  def self.conn
+    conn = Faraday.new(
+      url: 'https://api.zoom.us',
+      headers: {
+        'Content-Type' => 'application/json',
+        'authorization' => "Bearer #{generate_jwt}"
       }
-      JWT.encode payload, ENV['ZOOM_API_SECRET'], 'HS256'
-    end
+    )
+  end
 
+  def self.generate_jwt
+    payload = {
+      "iss": ENV['ZOOM_API_KEY'],
+      "exp": Time.now.to_i + 5
+    }
+    JWT.encode payload, ENV['ZOOM_API_SECRET'], 'HS256'
+  end
+
+  def self.parse_response(response)
+    JSON.parse(response.body, symbolize_names: true)
+  end
 end
