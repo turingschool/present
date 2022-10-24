2b544df98f4f6a4b13d16bda686abda2ba683482
Brian Zanti <brianzanti@gmail.com>
2022-10-24 17:40:34 -0500

    removes uniqueness validation for zoom meeeting id

--- spec/models/attendance_spec.rb
+++ spec/models/attendance_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Attendance, type: :model do
   describe 'validations' do
     it {should validate_presence_of :zoom_meeting_id}
-    it {should validate_uniqueness_of :zoom_meeting_id}
+    # it {should validate_uniqueness_of :zoom_meeting_id}
   end
 
   describe 'relationships' do
