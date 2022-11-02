f8434fdda4d21c98634fb2742d1a93d4b9a038ed
Chris Simmons <chris@turing.edu>
2022-11-02 17:20:30 -0400

    Adds by_attendance_status method, with tests and view

--- app/models/student_attendance.rb
+++ app/models/student_attendance.rb
@@ -10,6 +10,12 @@ class StudentAttendance < ApplicationRecord
       .order("last_name")
   end
 
+  def self.by_attendance_status
+    joins(:student)
+      .select("students.*, student_attendances.*, split_part(students.name, ' ', 2) as last_name")
+      .order("status DESC, last_name ASC")
+  end
+
   def visiting_student?
     return true if student.turing_module.nil?
     return true if student.turing_module != self.attendance.turing_module
