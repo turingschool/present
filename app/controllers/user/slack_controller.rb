0535d57f2fbbbf504c6b520ee35c9d86e1a40830
Meg Stang <mstang15@gmail.com>
2023-01-06 15:15:32 -0700

    Add edge case for invalid Cohort Id

--- app/controllers/user/slack_controller.rb
+++ app/controllers/user/slack_controller.rb
@@ -3,8 +3,13 @@ class User::SlackController < ApplicationController
         slack_channel_id = params[:slack_channel_id]
         turing_module = TuringModule.find(params[:turing_module_id])
         channel_members = SlackFacade.get_and_create_slack_members(slack_channel_id, turing_module)
-        flash[:success] = "#{turing_module.slack_members.count} members from Cohort have been imported"
-        redirect_to turing_module_path(turing_module)
+        if channel_members
+            flash[:success] = "#{turing_module.slack_members.count} members from Cohort have been imported"
+            redirect_to turing_module_path(turing_module)
+        else
+            flash[:error] = "Please provide a valid channel id."
+            redirect_to turing_module_slack_channel_import_path(turing_module)
+        end
     end 
 
     def new 
--- app/facades/slack_facade.rb
+++ app/facades/slack_facade.rb
@@ -1,6 +1,7 @@
 class SlackFacade 
    def self.get_and_create_slack_members(channel_id, turing_module) 
         channel_members = SlackService.get_channel_members(channel_id)[:data]
+        return false if !channel_members
         channel_members.each do |member|
             turing_module.slack_members.create(member[:attributes]) if !turing_module.slack_members.exists?(slack_user_id: member[:attributes][:slack_user_id])
         end 
