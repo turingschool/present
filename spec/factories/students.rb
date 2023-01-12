6cd9479027164271af30cb274bb06cfd8c22e9fe
Meg Stang <mstang15@gmail.com>
2023-01-11 20:07:58 -0700

    Add factory for students with slack ids

--- spec/factories/students.rb
+++ spec/factories/students.rb
@@ -3,5 +3,9 @@ FactoryBot.define do
     turing_module
     sequence(:zoom_id) { |n| "<zoom_id>_#{n}" }
     name { Faker::Name.name }
+
+    factory :student_with_slack_id do
+      sequence(:slack_id) {|n| "<slack_id_#{n}"}
+    end
   end
 end
