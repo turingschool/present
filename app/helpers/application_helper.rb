970ae834491d8683038ebafdbd78dc278a86a538
Meg Stang <mstang15@gmail.com>
2023-01-11 20:05:08 -0700

    Clean up comments

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,12 +3,5 @@ module ApplicationHelper
             a = turing_module.slack_members.sort_by {|x| x.name}.map do |slack_member|
                 [slack_member.name,slack_member.slack_user_id]
             end
-            # binding.pry
-        # [
-        #   ['Air Nomads', 'air+nomads'],
-        #   ['Earth Kingdom', 'earth+kingdom'],
-        #   ['Fire Nation', 'fire+nation'],
-        #   ['Water Tribes', 'water+tribes'],
-        # ]
       end
 end
