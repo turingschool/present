cb18b51b70ae6e578f43044de2e495aca52e591f
Brian Zanti <brianzanti@gmail.com>
2023-02-14 22:31:04 -0600

    adds additional expectations to find_or_create_from_participant test

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -31,16 +31,20 @@ RSpec.describe Student, type: :model do
 
       it 'finds the students if they exist' do
         existing_student = Student.create(zoom_id: participant[:id], name: participant[:name])
+        expect(Student.count).to eq(1)
         found_student = Student.find_or_create_from_participant(participant)
         expect(found_student.id).to eq(existing_student.id)
         expect(found_student.name).to eq(existing_student.name)
         expect(found_student.zoom_id).to eq(existing_student.zoom_id)
+        expect(Student.count).to eq(1)
       end
 
       it 'creates the student with all their info if they do not exist' do
+        expect(Student.count).to eq(0)
         found_student = Student.find_or_create_from_participant(participant)
         expect(found_student.name).to eq(participant[:name])
         expect(found_student.zoom_id).to eq(participant[:id])
+        expect(Student.count).to eq(1)
       end
     end
   end
