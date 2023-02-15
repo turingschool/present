d58a2609ed91d3c353339260ecc105f289674de0
Brian Zanti <brianzanti@gmail.com>
2023-02-14 22:30:39 -0600

    adds rescue to deal with stale current user in session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,17 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user, :current_inning
 
+  rescue_from ActiveRecord::RecordNotFound, with: :clear_current_user
+
+  def clear_current_user(error)
+    if error.message == "Couldn't find User with 'id'=#{session[:user_id]}"
+      session.delete(:user_id)
+      redirect_to root_path
+    else
+      raise error
+    end
+  end
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
