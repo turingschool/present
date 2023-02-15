81719b11d1b78bd92821577be800f0963f94e71f
Brian Zanti <brianzanti@gmail.com>
2023-02-15 11:39:20 -0600

    implements fuzzy string search for best matching student names

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def find_jarow_match(student, student_list)
+    match = student_list.max_by do |name, id|
+      jarow = FuzzyStringMatch::JaroWinkler.create(:pure)
+      jarow.getDistance(student.name, name)
+    end
+    return match[1]
+  end
 end
