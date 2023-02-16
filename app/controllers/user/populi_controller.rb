ce92394e4fe3274cb79600139314a8797033b4d7
Brian Zanti <brianzanti@gmail.com>
2023-02-16 11:13:25 -0600

    refactor populi controller to use StringMatcher module

--- app/controllers/user/populi_controller.rb
+++ app/controllers/user/populi_controller.rb
@@ -1,18 +1,25 @@
 class User::PopuliController < User::BaseController
+  include StringMatcher
+
+  before_action :set_module
+
+  def set_module
+    @module = TuringModule.find(params[:turing_module_id])
+  end
+
   def new
     service = PopuliService.new
     current_term_id = service.get_current_academic_term[:response][:termid]
     @courses = service.get_courses(current_term_id)[:response][:course_instance]
-    jarow = FuzzyStringMatch::JaroWinkler.create(:pure)
-    @module = TuringModule.find(params[:turing_module_id])
-    @top_choice = @courses.max_by do |course|
-      jarow.getDistance(@module.name, course[:abbrv])
+    course_names = @courses.map {|course| course[:abbrv]}
+    match = find_jarow_match(@module.name, course_names)
+    @top_choice = @courses.find do |course|
+      course[:abbrv] == match
     end
     @courses -= [@top_choice]
   end  
 
   def create
-    @module = TuringModule.find(params[:turing_module_id])
     params[:populi_students].each do |student_id, populi_id|
       Student.update(student_id, {populi_id: populi_id})
     end
@@ -20,7 +27,6 @@ class User::PopuliController < User::BaseController
   end
 
   def match_students
-    @module = TuringModule.find(params[:turing_module_id])
     begin
       @populi_students = PopuliService.new.get_students(params[:course_id])[:response][:courseinstance_student].map do |student|
         PopuliStudent.from_populi(student)
