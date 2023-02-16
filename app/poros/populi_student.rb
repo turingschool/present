531d8e4c483610d10de275b295febe466f42a2db
Brian Zanti <brianzanti@gmail.com>
2023-02-16 10:24:32 -0600

    adds PopuliStudent poro

---
+++ app/poros/populi_student.rb
@@ -0,0 +1,18 @@
+class PopuliStudent
+  attr_reader :name, :personid
+
+  def initialize(name, personid)
+    @name = name
+    @personid = personid
+  end
+
+  def self.from_populi(populi_data)
+    name = full_name(populi_data[:first], populi_data[:last], populi_data[:preferred])
+    new(name, populi_data[:personid])
+  end
+
+  def self.full_name(first, last, preferred)
+    "#{first} #{"(#{preferred}) " if preferred}#{last}"
+  end
+end
+
