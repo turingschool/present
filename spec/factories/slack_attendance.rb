8565f6ad66e4b78ece3c7cf3541ce7ffdc6b6395
Meg Stang <mstang15@gmail.com>
2023-02-16 08:44:00 -0700

    Update slack_attendance factory to use time datatype rather than datetime

--- spec/factories/slack_attendance.rb
+++ spec/factories/slack_attendance.rb
@@ -2,8 +2,8 @@ FactoryBot.define do
     factory :slack_attendance do
         attendance
         sequence(:channel_id) { |n| "<slack_channel_#{n}>" }
-        sent_timestamp { Time.new('2021-12-10 23:10:49 UTC').to_datetime }
-        attendance_start_time { Time.new('2021-12-10 23:10:49 UTC').to_datetime }
+        sent_timestamp { Time.new('2021-12-10 23:10:49 UTC') }
+        attendance_start_time { Time.new('2021-12-10 23:10:49 UTC') }
     end
   end
   
