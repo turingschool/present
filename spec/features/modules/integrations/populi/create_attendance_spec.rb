5f0b6746d2eca929cf18a2cfbe7f29da7a70e896
Brian Zanti <brianzanti@gmail.com>
2023-02-23 18:23:52 -0600

    changes fixture files

--- spec/features/modules/integrations/populi/create_attendance_spec.rb
+++ spec/features/modules/integrations/populi/create_attendance_spec.rb
@@ -13,13 +13,13 @@ RSpec.describe 'taking attendance with Populi' do
       @student_6 = create(:student, turing_module: @mod, name: 'Mike C. (he/him) BE', populi_id: 24490150)
       @student_7 = create(:student, turing_module: @mod, name: 'Samuel C (He/Him) BE', populi_id: 24490123)
 
-      @test_zoom_meeting_id = 95490216907
+      @test_zoom_meeting_id = 96428502996
 
       stub_request(:get, "https://api.zoom.us/v2/report/meetings/#{@test_zoom_meeting_id}/participants?page_size=300") \
-      .to_return(body: File.read('spec/fixtures/zoom_meeting_participant_report.json'))
+      .to_return(body: File.read('spec/fixtures/participant_report_for_populi.json'))
 
       stub_request(:get, "https://api.zoom.us/v2/meetings/#{@test_zoom_meeting_id}") \
-      .to_return(body: File.read('spec/fixtures/zoom_meeting_details.json'))
+      .to_return(body: File.read('spec/fixtures/meeting_details_for_populi.json'))
 
       @test_module = create(:turing_module)
     end
