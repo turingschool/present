0afc2f4aba75a2aa5f2d1ee9cd455c48decc5387
Brian Zanti <brianzanti@gmail.com>
2023-03-08 22:40:05 -0600

    removes unused comments

--- spec/features/attendances/create_slack_attendance_spec.rb
+++ spec/features/attendances/create_slack_attendance_spec.rb
@@ -73,37 +73,4 @@ RSpec.describe 'Creating an Attendance' do
   end
 
   it 'sad path'
-  
-  # let(:expected_students){
-  #   [
-  #     Student.new(zoom_id: "VTOR3RckQRSDd5OeKkOfkQ", name: "Meg Stang (she/her)", slack_id: "UBJS832JG"), 
-  #     Student.new(zoom_id: "4zStqvqLStqmuqbAvC03kg", name: "Annie P.", slack_id: "U03MAN29L9W"), 
-  #     Student.new(zoom_id: "CVQTm5CtRKqf05swMFzC8Q", name: "BE Ashley T (she/her)", slack_id: "U03BM5HCCRY"), 
-  #     Student.new(zoom_id: "OUfhKnROQxufMTO8-RAS_Q", name: "James White (He/Him) BE 2208", slack_id: "U03LLTKBYUB"), 
-  #     Student.new(zoom_id: "uZrMY7y1StyThYLy5UXOUA", name: "Kevin T (He/Him)# BE", slack_id: "U02KTLG8WBZ"), 
-  #     Student.new(zoom_id: "xbYVHLWhR_aebK29zlPekw", name: "Madeline Mauser# BE", slack_id: "U0376KHBE78"), 
-  #     Student.new(zoom_id: "yKWJiIdTS7-MP6P2hgVMRQ", name: "Joseph H (He/Him)# BE", slack_id: "U03L266HAUV"), 
-  #     Student.new(zoom_id: "s94_3_CiQr6C1HMPD-r5Ow", name: "Emily P. (she/ her)# BE", slack_id: "U03KYFF1E0N"), 
-  #     Student.new(zoom_id: "1nzD92XMQKSrtdVXoqhEbQ", name: "Naomi Y (she/her)# BE", slack_id: "U03L6BK5PAB"),
-  #     Student.new(zoom_id: "er-o3-QvQgmHofSRBrBAKQ", name: "Amanda R (she/her)# BE",slack_id: "U03LUS2DCRE"),
-  #     Student.new(zoom_id: "LeA0VUwlQam1mWOIAYajMw", name: "Darby S (she/her)# BE",slack_id: "U03L7FKNX0C"),
-  #     Student.new(zoom_id: "O56xuW5YSQiQbvbhKPBNHg", name: "Rich K (he/him)# BE",slack_id: "U03LJ24QEPP"),
-  #     Student.new(zoom_id: "KzAlQssMTR62vR7HmRqlKQ", name: "Yuji K (he/him)# BE",slack_id: "U03LEBHFZBQ"),
-  #     Student.new(zoom_id: "YWhgOE11QWGWOgBIbBS4NA", name: "Sage S (she/her) BE",slack_id: "U02V1UU109Z"),
-  #     Student.new(zoom_id: "ZMu_HIL6Sg6AlbM6XKaQOw", name: "Michael M (he/him) BE",slack_id: "U022NF3D4SV"),
-  #     Student.new(zoom_id: "gKz01c9PQoO2-TfF0Nwqpw", name: "Lucas C. (he/him)# BE",slack_id: "U02SS907SAH"),
-  #     Student.new(zoom_id: "FXLrfOGnTle16PbCxvSUpA", name: "Gabe N (he/him)# BE",slack_id: "U03LPBPDR44"),
-  #     Student.new(zoom_id: "HcYrw04ZQZOU_9Se2eOlkw", name: "Alex M (He#Him) BE",slack_id: "U02ACB9A0B1"),
-  #     Student.new(zoom_id: "HXqZyYDmScO32b0HfdH9-w", name: "Bryan K (he/him)# BE",slack_id: "U03KQFESGAK"),
-  #     Student.new(zoom_id: "F87iJeJ4Rze7-MgJL--f0A", name: "Shawn L (He/Him) BE",slack_id: "U03EMJ8TK8S"),
-  #     Student.new(zoom_id: "J7GkxLUXRCGyDs4xCsoJIw", name: "Kristen Nestler",slack_id: "U03EMGP53EW"),
-  #     Student.new(zoom_id: "zkl1WX2MQ5GXvbGRBy1bDw", name: "Will W (he/him)# BE",slack_id: "U03KQFD5WJK"),
-  #     Student.new(zoom_id: "Azz7feqVS9eDwPZdt7eX7Q", name: "Mostafa S (he/him)# BE",slack_id: "U03L53VSC1Y"),
-  #     Student.new(zoom_id: "LvCxeSlySTqGynb3jCnmlQ", name: "Kenz L (she/her)# BE",slack_id: "U03LLV9J8DQ"),
-  #     Student.new(zoom_id: "_QelgKtURaa3CmVX1pWrqw", name: "Sean Culliton (he/him) 2208 BE",slack_id: "U03LJ24VC0M"),
-  #     Student.new(zoom_id: "Q4QZvdc3QEiV7DMtak47Zg", name: "Astrid H (she/her)# BE",slack_id: "U03E70Z4F39"),
-  #     Student.new(zoom_id: "BeMgdDUNTWqKoXGs1c2-AA", name: "Eli Fuchsman",slack_id: "U03L53W3Q1Y")
-  #   ]
-  # }
-
 end 
