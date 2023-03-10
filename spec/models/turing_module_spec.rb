65ce0b25ee2f74991dcd3c819f36149c1854cf8a
Brian Zanti <brianzanti@gmail.com>
2023-03-09 19:37:19 -0600

    improves factory bot in models

--- spec/models/turing_module_spec.rb
+++ spec/models/turing_module_spec.rb
@@ -17,29 +17,25 @@ RSpec.describe TuringModule, type: :model do
   describe 'instance methods' do
     describe '#name' do
       it 'returns a combo of the module number and program' do
-        test_module = create(:turing_module, program: :FE, module_number: 3)
-        expect(test_module.name).to eq('FE Mod 3')
+        test_module = create(:setup_module)
+        expect(test_module.name).to eq('BE Mod 3')
       end
     end
 
     describe '#account_match_complete' do 
       it 'returns true if some students in a mod have slack ids, and some have zoom ids' do 
-        test_module = create(:fe1)
-        students = create_list(:student, 5, turing_module: test_module)
+        test_module = create(:setup_module)
 
         expect(test_module.account_match_complete).to eq true 
       end 
       it 'returns false if some students in a mod have slack ids but none have zoom ids' do 
-        test_module = create(:fe1)
-        create_list(:student, 3, zoom_id: nil, turing_module: test_module)
-        create_list(:student, 2, slack_id: nil, zoom_id: nil, turing_module: test_module)
+        test_module = create(:turing_module)
         
         expect(test_module.account_match_complete).to eq false 
       end 
 
       it 'returns false if no students have slack or zoom ids' do 
-        test_module = create(:fe1)
-        students = create_list(:student, 5, slack_id: nil, zoom_id: nil, turing_module: test_module)
+        test_module = create(:turing_module)
 
         expect(test_module.account_match_complete).to eq false 
       end 
