d96e8924fdb32842931ded9830443c330f2f5139
Brian Zanti <brianzanti@gmail.com>
2023-03-11 20:57:12 -0600

    abstracts attendance#record

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -7,29 +7,36 @@ class Attendance < ApplicationRecord
   has_many :student_attendances, dependent: :destroy
   has_many :students, through: :student_attendances
 
+  def record(meeting, attendance_time)
+    self.transaction do
+      meeting.create_child_attendance_record(self)
+      student_attendances = take_participant_attendance(meeting.participants)
+      take_absentee_attendance
+    end
+  end
+
+  def take_participant_attendance(participants)
+    participants.each do |participant|
+      student = find_student_from_participant(participant)
+      next if student.nil?
+      student_attendances.create(student: student, status: participant.status)
+    end
+  end
+
+  def take_absentee_attendance
+    student_ids = student_attendances.pluck(:student_id)
+    absent_students = turing_module.students.where.not(id: student_ids)
+    absent_students.each do |student|
+      student_attendances.create(student: student, status: "absent")
+    end
+  end
+
   def find_student_from_participant(participant)
     if participant.class == ZoomParticipant
       zoom_alias = zoom_attendance.find_or_create_zoom_alias(participant.name)
       return zoom_alias.student if zoom_alias
     elsif participant.class == SlackThreadParticipant
       Student.find_by(slack_id: participant.id)
-    end
-  end
-
-  def record(meeting, attendance_time)
-    meeting.create_child_attendance_record(self)
-    meeting.participants.each do |participant|
-      student = find_student_from_participant(participant)
-      next if student.nil?
-      student_attendances.new(student: student, status: participant.status)
-    end
-    student_ids = student_attendances.map(&:student_id)
-    absent_students = turing_module.students.where.not(id: student_ids)
-    absent_students.each do |student|
-      student_attendances.new(student: student, status: "absent")
-    end
-    self.transaction do
-      student_attendances.each(&:save!)
     end
   end
 end
