9c403ab32605106e6d9b75193f5785fa9436482a
Brian Zanti <brianzanti@gmail.com>
2023-03-30 15:56:46 -0500

    refactors data setup for spec

--- spec/features/modules/show_spec.rb
+++ spec/features/modules/show_spec.rb
@@ -7,77 +7,44 @@ RSpec.describe 'Modules show page' do
   end
 
   it 'shows the modules attributes' do
-    
-    create_list(:student, 5, turing_module: @test_module)
-
     visit "/modules/#{@test_module.id}"
 
     expect(page).to have_content("#{@test_module.program} Mod #{@test_module.module_number}")
     expect(page).to have_content("#{@test_module.inning.name} inning")
   end
 
-  it 'shows the past zoom attendances for the module' do
+  context "with attendances" do
+    before :each do 
+      zoom_attendances = create_list(:zoom_attendance, 3, turing_module: @test_module)
+      slack_attendances = create_list(:slack_attendance, 3, turing_module: @test_module)
+      @attendances = zoom_attendances + slack_attendances
+    end
 
-    create_list(:student, 5, turing_module: @test_module)
+    it 'shows the past Zoom and Slack attendances for the module' do
+      visit "/modules/#{@test_module.id}"
 
-    attendances = create_list(:zoom_attendance, 3).map do |zoom_attendance|
-      zoom_attendance.attendance.update(turing_module: @test_module)
-      zoom_attendance.attendance
-    end 
-
-    visit "/modules/#{@test_module.id}"
-
-    within('#past-attendances') do
-      expect(page).to have_content('Past Attendances')
-      attendances.each do |attendance|
-        within("#attendance-#{attendance.id}") do
-          expect(page).to have_content(attendance.zoom_attendance.meeting_time)
-          expect(page).to have_content(attendance.zoom_attendance.meeting_title)
-          expect(page).to have_content(attendance.zoom_attendance.zoom_meeting_id)
+      within('#past-attendances') do
+        expect(page).to have_content('Past Attendances')
+        @attendances.each do |attendance|
+          within("#attendance-#{attendance.id}") do
+            expect(page).to have_content(attendance.meeting.title)
+            expect(page).to have_content(attendance.pretty_date)
+            expect(page).to have_content(attendance.pretty_time)
+          end
         end
       end
     end
-  end
 
-  it 'shows the past slack attendances for the module' do
+    it "has a link to each attendance's show page" do
+      test_attendance = @attendances.second
 
-    create_list(:student, 5, turing_module: @test_module)
-
-    attendances = create_list(:slack_attendance, 3).map do |slack_attendance|
-      slack_attendance.attendance.update(turing_module: @test_module)
-      slack_attendance.attendance
-    end 
-
-    visit "/modules/#{@test_module.id}"
-
-    within('#past-attendances') do
-      expect(page).to have_content('Past Attendances')
-      attendances.each do |attendance|
-        within("#attendance-#{attendance.id}") do
-          # expect(page).to have_link(attendance.slack_attendance.pretty_time_date, href: attendance_path(attendance))
-          expect(page).to have_content(attendance.slack_attendance.attendance_start_time)
+      visit "/modules/#{@test_module.id}"
+      
+      within('#past-attendances') do
+        within("#attendance-#{test_attendance.id}") do
+          click_link(test_attendance.meeting.title)
+          expect(current_path).to eq("/attendances/#{test_attendance.id}")
         end
-      end
-    end
-  end
-
-  it "has a link to each attendance's show page" do
-
-    create_list(:student, 5, turing_module: @test_module)
-
-    attendances = create_list(:zoom_attendance, 3).map do |zoom_attendance|
-      zoom_attendance.attendance.update(turing_module: @test_module)
-      zoom_attendance.attendance
-    end 
-
-    test_attendance = attendances[1]
-
-    visit "/modules/#{@test_module.id}"
-
-    within('#past-attendances') do
-      within("#attendance-#{test_attendance.id}") do
-        click_link(test_attendance.zoom_attendance.meeting_title)
-        expect(current_path).to eq("/attendances/#{test_attendance.id}")
       end
     end
   end
