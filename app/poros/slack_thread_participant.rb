f1f3e9f882d7a531f3a0a25d2f152cd74cf6f3ff
Brian Zanti <brianzanti@gmail.com>
2023-03-30 17:35:35 -0500

    refactors slack participant to match zoom

--- app/poros/slack_thread_participant.rb
+++ app/poros/slack_thread_participant.rb
@@ -1,26 +1,22 @@
 class SlackThreadParticipant
-  attr_reader :slack_id
+  attr_reader :slack_id, :status, :join_time
 
   SLACK_TARDY_GRACE_PERIOD_IN_MINUTES = 5
   SLACK_ABSENT_GRACE_PERIOD_IN_MINUTES = 30
 
   def initialize(reply_info)
     @slack_id = reply_info[:slack_id]
-    @reply_time = Time.parse(reply_info[:reply_timestamp])
+    @join_time = Time.parse(reply_info[:reply_timestamp])
   end
 
-  def student
-    Student.find_by(slack_id: slack_id)
-  end
-
-  def attendance_status(attendance_time)
-    minutes_passed_start_time = (@reply_time - attendance_time)/60.0
+  def assign_status!(attendance_time)
+    minutes_passed_start_time = (join_time - attendance_time)/60.0
     if minutes_passed_start_time > SLACK_ABSENT_GRACE_PERIOD_IN_MINUTES
-      return 'absent' 
+      @status = 'absent' 
     elsif minutes_passed_start_time > SLACK_TARDY_GRACE_PERIOD_IN_MINUTES
-      return 'tardy'
+      @status = 'tardy'
     else
-      return 'present' 
+      @status = 'present' 
     end
   end
 end
