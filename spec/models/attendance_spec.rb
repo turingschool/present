1abbbd7f87d92602a43caf5910524881f90e812e
Brian Zanti <brianzanti@gmail.com>
2023-04-03 16:44:57 -0500

    removes test for pretty_time and date

--- spec/models/attendance_spec.rb
+++ spec/models/attendance_spec.rb
@@ -12,17 +12,4 @@ RSpec.describe Attendance, type: :model do
     it {should have_many :student_attendances}
     it {should have_many(:students).through(:student_attendances)}
   end
-
-  describe 'instance methods' do
-    describe 'pretty_time' do
-      it 'returns hours and minutes in mountain time' do
-        attendance_1 = create(:attendance, attendance_time: DateTime.parse('2021-12-17T16:00:00Z')) # 9am mountain
-        attendance_2 = create(:attendance, attendance_time: DateTime.parse('2021-12-17T20:00:00Z')) # 1pm mountain
-        attendance_3 = create(:attendance, attendance_time: DateTime.parse('2021-12-17T18:59:59Z')) # 11:59:59am mountain
-        expect(attendance_1.pretty_time).to eq('9:00 AM')
-        expect(attendance_2.pretty_time).to eq('1:00 PM')
-        expect(attendance_3.pretty_time).to eq('11:59 AM')
-      end
-    end
-  end
 end
