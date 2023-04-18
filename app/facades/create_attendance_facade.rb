908c9d288fe7338a365501cc3b45774d7acc8866
Brian Zanti <brianzanti@gmail.com>
2023-04-18 11:44:05 -0500

    removes unused method

--- app/facades/create_attendance_facade.rb
+++ app/facades/create_attendance_facade.rb
@@ -8,13 +8,6 @@ class CreateAttendanceFacade
     return attendance
   end
 
-  def self.retake_attendance(attendance)
-    attendance.student_attendances.destroy_all
-    meeting = create_meeting(attendance.zoom_attendance.zoom_meeting_id)
-    attendance.record(meeting)
-    # update_populi(attendance, course_id, populi_meeting.id)
-  end
-
   def self.create_meeting(meeting_url)
     if meeting_url.downcase.include? 'slack'
       @meeting = SlackThread.from_message_link(meeting_url)
