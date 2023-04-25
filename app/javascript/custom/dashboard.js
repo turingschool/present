47a955c5d723d67a60874515fde86652a4531ea3
Brian Zanti <brianzanti@gmail.com>
2023-04-24 23:26:36 -0500

    confirmation for redo setup

--- app/javascript/custom/dashboard.js
+++ app/javascript/custom/dashboard.js
@@ -5,4 +5,11 @@ ready(() => {
     const moduleID = event.target.selectedOptions[0].value
     window.location.href = `/modules/${moduleID}`
   });
+  
+  document.getElementById("redo-setup").addEventListener("click", event => {
+    event.preventDefault();
+    if (confirm("Are you sure?") == true) {
+      window.location = event.target.href
+    } 
+  });
 });
