a4a70821b91547ed09483c13f6e0306406b2c821
Brian Zanti <brianzanti@gmail.com>
2023-04-25 17:50:33 -0500

    adds guards for js event listeners

--- app/javascript/custom/dashboard.js
+++ app/javascript/custom/dashboard.js
@@ -1,15 +1,21 @@
 import ready from "custom/main";
 
 ready(() => {
-  document.getElementById("turing-module-select").addEventListener("change", event => {
-    const moduleID = event.target.selectedOptions[0].value
-    window.location.href = `/modules/${moduleID}`
-  });
+  let element = document.getElementById("turing-module-select");
+  if (element !== null) {
+    document.getElementById("turing-module-select").addEventListener("change", event => {
+      const moduleID = event.target.selectedOptions[0].value
+      window.location.href = `/modules/${moduleID}`
+    });
+  }
   
-  document.getElementById("redo-setup").addEventListener("click", event => {
-    event.preventDefault();
-    if (confirm("Are you sure?") == true) {
-      window.location = event.target.href
-    } 
-  });
+  element = document.getElementById("redo-setup");
+  if (element !== null) {
+    document.getElementById("redo-setup").addEventListener("click", event => {
+      event.preventDefault();
+      if (confirm("Are you sure?") == true) {
+        window.location = event.target.href
+      } 
+    });
+  }
 });
