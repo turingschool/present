570513a70d42bd909f2be06c4e126598e0347bf2
Brian Zanti <brianzanti@gmail.com>
2023-04-25 17:50:00 -0500

    reenables turbo and disables on take attendance button

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import { Turbo } from "@hotwired/turbo-rails"
-Turbo.session.drive = false
 import "controllers"
 import "custom/main"
 import "custom/sortable"
