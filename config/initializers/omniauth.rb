067039d5c01bc327bb3a6bf675fc7f458ba6957d
Brian Zanti <brianzanti@gmail.com>
2023-05-10 12:30:49 -0500

    updates oauth domain names

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,19 +2,18 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   if Rails.env.production?
     provider :google_oauth2, ENV['GOOGLE_OAUTH_CLIENT_ID'], ENV['GOOGLE_OAUTH_CLIENT_SECRET'],
     {
-      redirect_uri: 'https://turing-present.herokuapp.com/auth/google_oauth2/callback',
+      redirect_uri: 'https://present.turing.edu/auth/google_oauth2/callback',
       scope: 'spreadsheets,email',
       access_type: 'offline'
     }
   elsif Rails.env.staging?
     provider :google_oauth2, ENV['GOOGLE_OAUTH_CLIENT_ID'], ENV['GOOGLE_OAUTH_CLIENT_SECRET'],
     {
-      redirect_uri: 'https://turing-present-staging.herokuapp.com/auth/google_oauth2/callback',
+      redirect_uri: 'https://present-staging.turing.edu/auth/google_oauth2/callback',
       scope: 'spreadsheets,email',
       access_type: 'offline'
     }
   else
-    
     provider :google_oauth2, ENV['GOOGLE_OAUTH_CLIENT_ID'], ENV['GOOGLE_OAUTH_CLIENT_SECRET'],
     {
       redirect_uri: 'http://localhost:3000/auth/google_oauth2/callback',
