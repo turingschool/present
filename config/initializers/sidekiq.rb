7f3da29a43e34f7a003339d4b821523ec9533f44
Brian Zanti <brianzanti@gmail.com>
2023-05-11 17:00:45 -0500

    formatting

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -5,9 +5,9 @@ Sidekiq.configure_server do |config|
     }
   end
 
-  Sidekiq.configure_client do |config|
-    config.redis = {
-        url: ENV["REDIS_URL"],
-        ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
-    }
-  end
+Sidekiq.configure_client do |config|
+  config.redis = {
+      url: ENV["REDIS_URL"],
+      ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+  }
+end
