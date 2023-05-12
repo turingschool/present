203756e755b73f8d427c6941094b31b511d2a2e4
Brian Zanti <brianzanti@gmail.com>
2023-05-12 09:40:26 -0500

    fixes typo

--- app/controllers/user/populi_transfer_controller.rb
+++ app/controllers/user/populi_transfer_controller.rb
@@ -6,7 +6,7 @@ class User::PopuliTransferController < User::BaseController
   def create
     @attendance = Attendance.find(params[:attendance_id])
     PopuliTransferJob.perform_async(@attendance.id)
-    flash[:success] = "Transfering attendance to Populi. Please confirm in Populi that attendance is accurate."
+    flash[:success] = "Transferring attendance to Populi. Please confirm in Populi that attendance is accurate."
     redirect_to @attendance
   end
 end
