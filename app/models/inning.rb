e06314c08e4ea958df903565e0b4ca998ea5d146
Brian Zanti <brianzanti@gmail.com>
2023-05-17 18:16:00 -0500

    fixes honeybadger notify

--- app/models/inning.rb
+++ app/models/inning.rb
@@ -16,8 +16,12 @@ class Inning < ApplicationRecord
   def check_presence_for_students
     students.each do |student|
       response = SlackApiService.get_presence(student.slack_id)
-      student.slack_presence_checks.create(presence: response[:presence], check_time: Time.now)
-      HoneyBadger.notify(response.to_s) unless response[:ok]
+      if response[:ok]
+        student.slack_presence_checks.create(presence: response[:presence], check_time: Time.now)
+      else
+        # We want to be notified if any API call to get a user's presence fails for any reason
+        Honeybadger.notify(response.to_s)
+      end 
     end
   end
 end
