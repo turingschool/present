b1b3cb1870f21dfacb26370208f43a34308a012d
Brian Zanti <brianzanti@gmail.com>
2023-05-18 16:37:15 -0500

    logs the same check time for every student when doing a presence check

--- app/models/inning.rb
+++ app/models/inning.rb
@@ -14,10 +14,11 @@ class Inning < ApplicationRecord
   end
 
   def check_presence_for_students
+    check_time = Time.now
     students.each do |student|
       response = SlackApiService.get_presence(student.slack_id)
       if response[:ok]
-        student.slack_presence_checks.create(presence: response[:presence], check_time: Time.now)
+        student.slack_presence_checks.create(presence: response[:presence], check_time: check_time)
       else
         # We want to be notified if any API call to get a user's presence fails for any reason
         Honeybadger.notify("Slack Response: #{response.to_s}, Student Slack ID: #{student.slack_id.to_s}")
