272900bf4c3a149e0a1b3aa1c4f3d4736ed4e632
Brian Zanti <brianzanti@gmail.com>
2023-05-18 16:42:14 -0500

    adds rate limiting for slack presence check

--- app/models/inning.rb
+++ app/models/inning.rb
@@ -15,14 +15,17 @@ class Inning < ApplicationRecord
 
   def check_presence_for_students
     check_time = Time.now
-    students.each do |student|
-      response = SlackApiService.get_presence(student.slack_id)
-      if response[:ok]
-        student.slack_presence_checks.create(presence: response[:presence], check_time: check_time)
-      else
-        # We want to be notified if any API call to get a user's presence fails for any reason
-        Honeybadger.notify("Slack Response: #{response.to_s}, Student Slack ID: #{student.slack_id.to_s}")
-      end 
+    students.each_slice(50) do |student_slice|
+      student_slice.each do |student|
+        response = SlackApiService.get_presence(student.slack_id)
+        if response[:ok]
+          student.slack_presence_checks.create(presence: response[:presence], check_time: check_time)
+        else
+          # We want to be notified if any API call to get a user's presence fails for any reason
+          Honeybadger.notify("Slack Response: #{response.to_s}, Student Slack ID: #{student.slack_id.to_s}")
+        end 
+      end
+      sleep(1.minute)
     end
   end
 end
