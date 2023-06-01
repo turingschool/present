0bbce15524ecdf87b8606ed26d790d7b0d9ec754
Brian Zanti <brianzanti@gmail.com>
2023-06-01 14:18:52 -0500

    adds environment variable switch for presence monitoring

--- app/sidekiq/presence_monitoring_job.rb
+++ app/sidekiq/presence_monitoring_job.rb
@@ -2,6 +2,8 @@ class PresenceMonitoringJob
   include Sidekiq::Job
 
   def perform
-    Inning.find_by(current: true).check_presence_for_students
+    if ENV["PRESENCE_MONTIORING"]
+      Inning.find_by(current: true).check_presence_for_students
+    end
   end
 end
