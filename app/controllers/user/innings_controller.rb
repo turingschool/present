d770684bcb679eb1c9b1cfd603a88c4b34f02e6f
Brian Zanti <brianzanti@gmail.com>
2023-06-07 16:20:22 -0500

    removes unused controller actions

--- app/controllers/user/innings_controller.rb
+++ app/controllers/user/innings_controller.rb
@@ -1,26 +1,5 @@
 class User::InningsController < User::BaseController
-    def show
-        @inning = Inning.find(params[:id])
-    end
-
-    def create
-        new_inning = Inning.create(inning_params)
-        redirect_to inning_path(new_inning)
-    end
-
-    def index
-        @innings = Inning.order_by_name
-    end
-
-    def update
-        inning = Inning.find(params[:id])
-        inning.make_current_inning
-        redirect_to request.referer
-    end
-
-    private
-
-    def inning_params
-        params.require(:inning).permit(:name)
-    end
+  def show
+    @inning = Inning.find(params[:id])
+  end
 end
