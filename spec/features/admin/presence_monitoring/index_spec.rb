f41f2753b7409f5ebaad656fcd5831e857755b7b
Brady Rohrig <brady.rohrig@gmail.com>
2023-07-18 19:43:28 -0700

    wrote test for happy path of first page of slack_presence checks, paginated

--- spec/features/admin/presence_monitoring/index_spec.rb
+++ spec/features/admin/presence_monitoring/index_spec.rb
@@ -19,4 +19,24 @@ RSpec.describe 'Presence Monitoring Index' do
       expect(page).to have_content(check.presence)
     end
   end
+
+  it 'displays the slack presence checks in a paginated manner, with only 50 per page' do
+    checks_one = create_list(:slack_presence_check, 49)
+    first_page_last = create(:slack_presence_check)
+    second_page_first = create(:slack_presence_check)
+    checks_two = create_list(:slack_presence_check, 48)
+    second_page_last = create(:slack_presence_check)
+    third_page_first = create(:slack_presence_check)
+
+    visit '/admin/slack_presence_checks'
+    
+    expect(page).to have_content(first_page_last.student.name)
+    expect(page).to have_content(first_page_last.student.slack_id)
+    expect(page).to_not have_content(second_page_first.student.name)
+    expect(page).to_not have_content(second_page_first.student.slack_id)
+    expect(page).to_not have_content(second_page_last.student.name)
+    expect(page).to_not have_content(second_page_last.student.slack_id)
+    expect(page).to_not have_content(third_page_first.student.name)
+    expect(page).to_not have_content(third_page_first.student.slack_id)
+  end
 end
