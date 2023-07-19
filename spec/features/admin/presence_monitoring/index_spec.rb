25fbc2497563d8190c808b8f2aa7a901856f48b3
Brady Rohrig <brady.rohrig@gmail.com>
2023-07-18 19:59:24 -0700

    test: wrote test for presence of navigation buttons for paginated display

--- spec/features/admin/presence_monitoring/index_spec.rb
+++ spec/features/admin/presence_monitoring/index_spec.rb
@@ -20,23 +20,38 @@ RSpec.describe 'Presence Monitoring Index' do
     end
   end
 
-  it 'displays the slack presence checks in a paginated manner, with only 50 per page' do
-    checks_one = create_list(:slack_presence_check, 49)
-    first_page_last = create(:slack_presence_check)
-    second_page_first = create(:slack_presence_check)
-    checks_two = create_list(:slack_presence_check, 48)
-    second_page_last = create(:slack_presence_check)
-    third_page_first = create(:slack_presence_check)
+  describe 'pagination' do
+    before(:each) do
+      @checks_one = create_list(:slack_presence_check, 49)
+      @first_page_last = create(:slack_presence_check)
+      @second_page_first = create(:slack_presence_check)
+      @checks_two = create_list(:slack_presence_check, 48)
+      @second_page_last = create(:slack_presence_check)
+      @third_page_first = create(:slack_presence_check)
+    end
 
-    visit '/admin/slack_presence_checks'
-    
-    expect(page).to have_content(first_page_last.student.name)
-    expect(page).to have_content(first_page_last.student.slack_id)
-    expect(page).to_not have_content(second_page_first.student.name)
-    expect(page).to_not have_content(second_page_first.student.slack_id)
-    expect(page).to_not have_content(second_page_last.student.name)
-    expect(page).to_not have_content(second_page_last.student.slack_id)
-    expect(page).to_not have_content(third_page_first.student.name)
-    expect(page).to_not have_content(third_page_first.student.slack_id)
+    it 'displays the slack presence checks in a paginated manner, with only 50 per page' do
+      visit '/admin/slack_presence_checks'
+
+      expect(page).to have_content(@first_page_last.student.name)
+      expect(page).to have_content(@first_page_last.student.slack_id)
+      expect(page).to_not have_content(@second_page_first.student.name)
+      expect(page).to_not have_content(@second_page_first.student.slack_id)
+      expect(page).to_not have_content(@second_page_last.student.name)
+      expect(page).to_not have_content(@second_page_last.student.slack_id)
+      expect(page).to_not have_content(@third_page_first.student.name)
+      expect(page).to_not have_content(@third_page_first.student.slack_id)
+    end
+
+    it 'has navigation buttons that move between the pages' do
+      visit '/admin/slack_presence_checks'
+
+      expect(page).to have_link('Next', href: '/admin/slack_presence_checks/page/2')
+      expect(page).to have_link('2', href: '/admin/slack_presence_checks/page/2')
+      expect(page).to have_link('3', href: '/admin/slack_presence_checks/page/3')
+      expect(page).to have_link('Last', href: '/admin/slack_presence_checks/page/3')
+    end
   end
+
+
 end
