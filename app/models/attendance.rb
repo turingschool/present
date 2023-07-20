1fa490d6d34ba78288cf2a3999a33cc4cd07cf6b
Brian Zanti <brianzanti@gmail.com>
2023-07-20 15:04:14 -0500

    logs update attendance response from populi

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -53,6 +53,7 @@ class Attendance < ApplicationRecord
     course_id = self.turing_module.populi_course_id
     student_attendances.includes(:student).each do |student_attendance|
       response = service.update_student_attendance(course_id, populi_meeting_id, student_attendance.student.populi_id, student_attendance.status)
+      Rails.logger.info "Update Attendance Respond: #{response.to_s}"
       unless response[:response][:result] == "UPDATED"
         raise AttendanceUpdateError.new("UPDATE FAILED. Student: #{student_attendance.student.populi_id}, status: #{student_attendance.status}, response: #{response.to_s}") 
       end
