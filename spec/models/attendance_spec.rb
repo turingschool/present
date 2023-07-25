cad182c188d494206e462b913488c94a223349a6
Max MacGillivray <mary.macgillivray@gmail.com>
2023-07-25 11:52:03 -0700

    Create test space for each of the instance methods, wrote brief plan for 'update_time' test.

--- spec/models/attendance_spec.rb
+++ spec/models/attendance_spec.rb
@@ -14,10 +14,40 @@ RSpec.describe Attendance, type: :model do
   end
   
   describe 'instance methods' do
-    it '' do
+    xit '#record' do
       
     end
     
+    xit '#rerecord' do
+      
+    end
     
+    xit '#take_participant_attendance' do
+      
+    end
+    
+    xit '#take_absentee_attendance' do
+      
+    end
+    
+    xit '#update_time(time)' do
+      # def update_time(time)
+      #   hour = time.split(":").first
+      #   minutes = time.split(":").last
+      #   new_time = attendance_time.in_time_zone('Mountain Time (US & Canada)').change(hour: hour, min: minutes)
+      #   self.update!(attendance_time: new_time)
+      # end
+      
+      # create instance w/ timestamp
+      # Update the time of the instance
+    end
+    
+    xit '#count_status(status)' do
+      
+    end
+    
+    xit '#transfer_to_populi!(populi_meeting_id)' do
+      
+    end
   end
 end
