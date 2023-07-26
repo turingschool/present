a912ee00961ea958afb58b6e8fe16b3568eb513d
Meg Stang <mstang15@gmail.com>
2023-07-25 22:01:29 -0600

    Add direction column to zoom_meetings table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_26_035154) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_26_040041) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -112,6 +112,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_26_035154) do
     t.string "meeting_id"
     t.string "title"
     t.datetime "start_time", precision: nil
+    t.integer "duration"
   end
 
   add_foreign_key "attendances", "turing_modules"
