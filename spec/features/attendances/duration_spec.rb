29c7bd41108939fa8605cf8bc729f6d71ae63fae
Brian Zanti <brianzanti@gmail.com>
2023-08-28 15:19:46 -0600

    updates expectations and fixtures for duration tests

--- spec/features/attendances/duration_spec.rb
+++ spec/features/attendances/duration_spec.rb
@@ -7,10 +7,10 @@ RSpec.describe "Duration" do
     @test_zoom_meeting_id = 95490216907
 
     stub_request(:get, "https://api.zoom.us/v2/report/meetings/#{@test_zoom_meeting_id}/participants?page_size=300") \
-      .to_return(body: File.read('spec/fixtures/zoom/participant_report_for_name_matching.json'))
+      .to_return(body: File.read('spec/fixtures/zoom/participant_report_for_duration.json'))
 
     stub_request(:get, "https://api.zoom.us/v2/meetings/#{@test_zoom_meeting_id}") \
-      .to_return(body: File.read('spec/fixtures/zoom/meeting_details.json'))
+      .to_return(body: File.read('spec/fixtures/zoom/meeting_details_for_duration.json'))
 
     stub_request(:post, ENV['POPULI_API_URL']).
       with(body: {"task"=>"getCourseInstanceMeetings", "instanceID"=>@test_module.populi_course_id}).
@@ -25,38 +25,48 @@ RSpec.describe "Duration" do
     @attendance = Attendance.last
   end
 
-  it "Duration is 0% for students that don't have a zoom alias yet" do
+  it "Duration is 0 for students that don't have a zoom alias yet" do
     lacey = @test_module.students.find_by(name: 'Lacey Weaver')
     visit attendance_path(@attendance)
     
     within "#student-#{lacey.id}" do
-      expect(page).to have_content("0 Minutes (0%)")
+      expect(page).to have_content("0")
     end
   end
 
   it "Duration increases when an alias is assigned" do
-    j = @test_module.students.find_by(name: 'J Seymour')
+    lacey = @test_module.students.find_by(name: 'Lacey Weaver')
+
     visit attendance_path(@attendance)
-    within "#student-#{j.id}" do
-      expect(page).to have_content("0%")
+    within "#student-#{lacey.id}" do
+      expect(page).to have_content("0")
     end
 
-    within "#student-aliases-#{j.id}" do
-      select("J (he/they) BE")
+    within "#student-aliases-#{lacey.id}" do
+      select("Lacey Weaver")
       click_button "Save Zoom Alias"
     end
 
-    within "#student-#{j.id}" do
-      expect(page).to have_content("52%")
+    within "#student-#{lacey.id}" do
+      expect(page).to have_content("35")
     end
 
-    within "#student-aliases-#{j.id}" do
-      select("J Seymour (he/they) BE")
+    within "#student-aliases-#{lacey.id}" do
+      select("Lacey Weaver (She/Her)")
       click_button "Save Zoom Alias"
     end
 
-    within "#student-#{j.id}" do
-      expect(page).to have_content("100%")
+    within "#student-#{lacey.id}" do
+      expect(page).to have_content("59")
+    end
+    
+    within "#student-aliases-#{lacey.id}" do
+      select("Lacey Weaver (She/Her, BE)")
+      click_button "Save Zoom Alias"
+    end
+
+    within "#student-#{lacey.id}" do
+      expect(page).to have_content("63")
     end
   end
   
@@ -65,7 +75,7 @@ RSpec.describe "Duration" do
     visit attendance_path(@attendance)
     
     within "#student-#{j.id}" do
-      expect(page).to have_content("0%")
+      expect(page).to have_content("0")
     end
 
     within "#student-aliases-#{j.id}" do
@@ -74,7 +84,7 @@ RSpec.describe "Duration" do
     end
 
     within "#student-#{j.id}" do
-      expect(page).to have_content("48%")
+      expect(page).to have_content("21")
     end
 
     within "#student-aliases-#{j.id}" do
@@ -83,7 +93,7 @@ RSpec.describe "Duration" do
 
     within "#student-#{j.id}" do
       select("J (he/they) BE")
-      expect(page).to have_content("100%")
+      expect(page).to have_content("60")
     end
   end
 end
