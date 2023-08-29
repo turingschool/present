b9f52114df0797c969df17048aed3334ddd93087
Thomas Hawley <thawley22@turing.edu>
2023-08-29 11:29:11 -0600

    update: Add traits to Inning factory
    Co-authored-by: Alejandro Lopez
    121198380+AlejandroLopez1992@users.noreply.github.com
    Co-authored-by: Alec Kapicak
    120225785+AlecKap@users.noreply.github.com

--- spec/factories/inning.rb
+++ spec/factories/inning.rb
@@ -2,5 +2,19 @@ FactoryBot.define do
   factory :inning do
     name { '2107' }
     current { true }
+    start_date {Date.today+2.weeks}
+    
+    trait :not_current_past do
+      current { false }
+      start_date {Date.today-5.weeks}
+    end
+    trait :is_current do
+      current { true }
+      start_date {Date.today-2.weeks}
+    end
+    trait :not_current_future do
+      current { false }
+      start_date {Date.today+2.weeks}
+    end
   end
 end
