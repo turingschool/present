526fecc677c9a1a7a2ecfdc8d1211fae004783b7
margdaly <margaret.daly@gmail.com>
2023-09-22 10:55:28 -0600

    Refactor: Used #make_current_inning to simplify job
    Co-authored-by: Alec Kapicak <120225785+AlecKap@users.noreply.github.com>
    Co-authored-by: Alejandro Lopez <121198380+AlejandroLopez1992@users.noreply.github.com>
    Co-authored-by: Thomas Hawley <120520954+thawley2@users.noreply.github.com>

--- app/sidekiq/inning_rollover_job.rb
+++ app/sidekiq/inning_rollover_job.rb
@@ -2,9 +2,8 @@ class InningRolloverJob
   include Sidekiq::Job
 
   def perform(inning_id)
-    Inning.current_to_false
     inning = Inning.find(inning_id)
-    inning.update(current: true)
+    inning.make_current_inning
     inning.create_turing_modules
     User.reset_modules
   end
