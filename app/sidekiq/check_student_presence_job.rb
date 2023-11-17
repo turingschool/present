40e85dca9d781c3141cc627c61512bdc00831e1d
Brian Zanti <brianzanti@gmail.com>
2023-11-17 14:44:03 -0700

    logs time for presence checks to complete

--- app/sidekiq/check_student_presence_job.rb
+++ app/sidekiq/check_student_presence_job.rb
@@ -2,6 +2,11 @@ class CheckStudentPresenceJob
   include Sidekiq::Job
 
   def perform(module_id)
-    TuringModule.find(module_id).check_presence_for_students!
+    start = Time.now
+    mod = TuringModule.find(module_id)
+    mod.check_presence_for_students!
+    end_time = Time.now
+    time = (end_time - start).round
+    Rails.logger.info "Checked Presence for Students in #{mod.name} id=#{mod.id} in #{time} seconds"
   end
 end
