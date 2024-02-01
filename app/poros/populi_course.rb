7c71c3198b521d87d2da5d5693d30eccef90aec4
AlecKap <120225785+AlecKap@users.noreply.github.com>
2024-02-01 16:10:03 -0700

    update: Update populi course poro to accomodate for new incomming data
    Co-authored-by: Margaret Daly <121778028+margdaly@users.noreply.github.com>
    Co-authored-by: Alejandro Lopez <121198380+AlejandroLopez1992@users.noreply.github.com>

--- app/poros/populi_course.rb
+++ app/poros/populi_course.rb
@@ -2,7 +2,7 @@ class PopuliCourse
   attr_reader :name, :id
 
   def initialize(course_data)
-    @id = course_data[:instanceid]
+    @id = course_data[:course_offering_id]
     @name = "#{course_data[:abbrv]} - #{course_data[:name]}"
   end
 end
