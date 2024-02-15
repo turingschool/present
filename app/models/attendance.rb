0ab173839e5c5d4002c827d1ca838ff08cb72ce5
AlecKap <120225785+AlecKap@users.noreply.github.com>
2024-02-15 14:24:00 -0700

    feat: Update #transfer_to_populi! to account for new populi v2 response format
    Co-authored-by: Alejandro Lopez <121198380+AlejandroLopez1992@users.noreply.github.com>
    Co-authored-by: Margaret Daly <121778028+margdaly@users.noreply.github.com>

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -53,12 +53,11 @@ class Attendance < ApplicationRecord
         enrollment[:student_id] == student_attendance.student.populi_id.to_i
       end
       response = service.update_student_attendance(course_id, student_enrollment[:id], populi_meeting_id, student_attendance.status)
-      require 'pry'; binding.pry
       Rails.logger.info "Update Attendance Response: #{response.to_s}"
       begin
-        raise AttendanceUpdateError.new("UPDATE FAILED") unless response[:response][:object] == "UPDATED"
+        raise AttendanceUpdateError.new("UPDATE FAILED") unless response[:object] == "course_attendance"
       rescue AttendanceUpdateError, NoMethodError
-        Honeybadger.notify("UPDATE FAILED. Student: #{student_attendance.student.populi_id}, status: #{student_attendance.status}, response: #{response.to_s}")
+        Honeybadger.notify("UPDATE FAILED. Student: #{student_attendance.student.populi_id}, status: #{student_attendance.status}, response: #{response[:message]}")
       end
     end
   end
