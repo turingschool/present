b839ddc32059e1a7f1c0498f18f255da8dba9fcc
Alejandro Lopez <alejandrolopez1592@gmail.com>
2024-02-26 12:43:53 -0700

    feat: change how data of API response is accesed to adapt to updated Populi API calls
    Co-authored-by: Alec Kapicak <120225785+AlecKap@users.noreply.github.com>
    Co-authored-by: Margaret Daly <121778028+margdaly@users.noreply.github.com>

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -7,16 +7,16 @@ class Meeting < ApplicationRecord
 
   def closest_populi_meeting_to_start_time(course_id)
     # need to restructure to access new API object
-    meeting_data = PopuliService.new.course_meetings(course_id)[:response][:meeting].min_by do |data|
-      (start_time.to_i - data[:start].to_datetime.to_i).abs
+    meeting_data = PopuliService.new.course_meetings(course_id)[:data].min_by do |data|
+      (start_time.to_i - data[:start_at].to_datetime.to_i).abs
     end
     PopuliMeeting.new(meeting_data)
   end
 
   def populi_meetings_on_same_day(course_id)
     meeting_day = start_time.to_date
-    meetings = PopuliService.new.course_meetings(course_id)[:response][:meeting].find_all do |data|
-      meeting_day == Date.parse(data[:start])
+    meetings = PopuliService.new.course_meetings(course_id)[:data].find_all do |data|
+      meeting_day == Date.parse(data[:start_at])
     end
     meetings.map{|data| PopuliMeeting.new(data)}
   end
