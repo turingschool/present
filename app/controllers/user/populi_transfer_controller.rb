fdd47cbc6e689ee1ca0de22d165632ca6dced8f0
Alejandro Lopez <alejandrolopez1592@gmail.com>
2024-04-16 09:29:44 -0600

    feat: Remove conditional statement that should no longer be necessary
    Co-authored-by: Alec Kapicak <120225785+AlecKap@users.noreply.github.com>
    Co-authored-by: Margaret Daly <121778028+margdaly@users.noreply.github.com>

--- app/controllers/user/populi_transfer_controller.rb
+++ app/controllers/user/populi_transfer_controller.rb
@@ -8,13 +8,8 @@ class User::PopuliTransferController < User::BaseController
 
   def create
     attendance = Attendance.find(params[:attendance_id])
-    if params[:populi_meeting_id].blank?
-      flash[:error] = "Something went wrong while retrieving the Populi meeting times. Please try again."
-      redirect_to new_attendance_populi_transfer_path(attendance)
-    else
-      PopuliTransferJob.perform_async(attendance.id, params[:populi_meeting_id])
-      flash[:success] = "Transferring attendance to Populi. This could take up to 5 minutes. Please confirm in Populi that the transfer was successful."
-      redirect_to attendance
-    end
+    PopuliTransferJob.perform_async(attendance.id, params[:populi_meeting_id])
+    flash[:success] = "Transferring attendance to Populi. This could take up to 5 minutes. Please confirm in Populi that the transfer was successful."
+    redirect_to attendance
   end
 end
