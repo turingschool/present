c7aaf5e842587e07342838f34f215c65c85f13f2
Alejandro Lopez <alejandrolopez1592@gmail.com>
2024-04-16 09:31:28 -0600

    fix: Add back in commented code in case it becomes needed again
    Co-authored-by: Alec Kapicak <120225785+AlecKap@users.noreply.github.com>
    Co-authored-by: Margaret Daly <121778028+margdaly@users.noreply.github.com>

--- app/controllers/user/populi_transfer_controller.rb
+++ app/controllers/user/populi_transfer_controller.rb
@@ -8,6 +8,13 @@ class User::PopuliTransferController < User::BaseController
 
   def create
     attendance = Attendance.find(params[:attendance_id])
+    # Commented out code is from the original implementation, based on Populi API upgrade :populi_meeting_id should no longer be blank
+    
+    # if params[:populi_meeting_id].blank?
+    #   flash[:error] = "Something went wrong while retrieving the Populi meeting times. Please try again."
+    #   redirect_to new_attendance_populi_transfer_path(attendance)
+    # else
+    # end
     PopuliTransferJob.perform_async(attendance.id, params[:populi_meeting_id])
     flash[:success] = "Transferring attendance to Populi. This could take up to 5 minutes. Please confirm in Populi that the transfer was successful."
     redirect_to attendance
