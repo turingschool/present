e64e5bd64379ad542a73a96923e243c397b07c12
AlecKap <120225785+AlecKap@users.noreply.github.com>
2024-04-17 13:06:32 -0600

    Add and configure VCR Gemfile
    Co-authored-by: Alejandro Lopez <121198380+AlejandroLopez1992@users.noreply.github.com>
    Co-authored-by: Margaret Daly <121778028+margdaly@users.noreply.github.com>

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -82,3 +82,11 @@ Shoulda::Matchers.configure do |config|
     with.library :rails
   end
 end
+
+VCR.configure do |config|
+  config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+  config.hook_into :webmock
+  config.filter_sensitive_data('POPULI_API_ACCESS_KEY') { ENV['POPULI_API_ACCESS_KEY'] }
+  config.default_cassette_options = { re_record_interval: 7.days }
+  config.configure_rspec_metadata!
+end
