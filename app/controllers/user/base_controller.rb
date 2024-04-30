f169cf38af5b207c8fc120501d751474b085adce
Alejandro Lopez <alejandrolopez1592@gmail.com>
2024-04-30 13:11:06 -0600

    feat: Add failure response via rescue clause for NoMethodError

--- app/controllers/user/base_controller.rb
+++ app/controllers/user/base_controller.rb
@@ -1,5 +1,6 @@
 class User::BaseController < ApplicationController
   before_action :verify_google_user
+  rescue_from NoMethodError, with: :no_api_response
 
   def verify_google_user
     if current_user.nil?
@@ -7,5 +8,11 @@ class User::BaseController < ApplicationController
     elsif !current_user.valid_google_user?
       render 'error/unauthorized'
     end
+  end
+
+  def no_api_response
+    @current_module = TuringModule.find(params[:turing_module_id])
+    flash[:error] = "The page you are trying to access is currently unavailable. This may be due to an API call failing. Please try again later."
+    redirect_to turing_module_path(@current_module)
   end
 end
