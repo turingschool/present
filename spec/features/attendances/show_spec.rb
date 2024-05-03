ae78ad27d106b2536a6dd65384250f47c66fbc24
AlecKap <120225785+AlecKap@users.noreply.github.com>
2024-05-03 11:46:21 -0600

    test: Refactor attendance show page tests and add test for zoom alias dropdown selector

--- spec/features/attendances/show_spec.rb
+++ spec/features/attendances/show_spec.rb
@@ -34,6 +34,7 @@ RSpec.describe 'attendance show page' do
 
     it "shows each students name, join time, and status" do
       visit "/attendances/#{@test_attendance.id}"
+
       within '#student-attendances' do
         @test_attendance.student_attendances.each do |student_attendance|
           within "#student-attendance-#{student_attendance.id}" do
@@ -49,7 +50,7 @@ RSpec.describe 'attendance show page' do
       end
     end
 
-    it "students are listed first by Status (absent, tardy, then present), then Name" do
+    it "students are listed first by Status (absent, tardy, then present), then Name" do 
       student_a = create(:student,  name: "Firstname Alastname", turing_module: @test_attendance.turing_module)
       student_z = create(:student, name: "Firstname Zlastname", turing_module: @test_attendance.turing_module)
       student_b = create(:student, name: "Firstname Blastname", turing_module: @test_attendance.turing_module)
@@ -58,12 +59,33 @@ RSpec.describe 'attendance show page' do
       create(:student_attendance, student: student_z, status: 'absent', attendance: @test_attendance)
       create(:student_attendance, student: student_b, status: 'tardy', attendance: @test_attendance)
       create(:student_attendance, student: student_c, status: 'absent', attendance: @test_attendance)
-
+  
       visit attendance_path(@test_attendance)
 
       expect(student_c.name).to appear_before(student_z.name)
       expect(student_z.name).to appear_before(student_b.name)
       expect(student_b.name).to appear_before(student_a.name)  
+    end
+
+    context 'has and can save zoom aliases for students and instructors' do
+      it "has a dropdown selector to save a new zoom alias" do
+        test_attendance = create(:attendance)
+        create_list(:student_attendance, 4, attendance: test_attendance, status: :tardy)
+        create_list(:student_attendance, 3, attendance: test_attendance, status: :absent)
+        create_list(:student_attendance, 7, attendance: test_attendance, status: :present)
+        create_list(:zoom_alias, 16, turing_module: test_attendance.turing_module) # 16 because 14 are students and 2 are instructors.
+        
+        visit "/attendances/#{test_attendance.id}"
+
+        within '#student-attendances' do
+          @test_attendance.student_attendances.each do |student_attendance|
+            within "#student-attendance-#{student_attendance.id}" do
+              expect(test_attendance.turing_module.unclaimed_aliases).to_not be_empty
+              expect(page).to have_select("student[zoom_alias]", options: test_attendance.turing_module.unclaimed_aliases.map { |alias_name| alias_name.name })
+            end
+          end
+        end
+      end
     end
 
     it 'applies css classes to all students based on status' do
@@ -72,7 +94,7 @@ RSpec.describe 'attendance show page' do
       create_list(:student_attendance, 4, attendance: test_attendance, status: :tardy)
       create_list(:student_attendance, 3, attendance: test_attendance, status: :absent)
       create_list(:student_attendance, 7, attendance: test_attendance, status: :present)
-
+      
       visit "/attendances/#{test_attendance.id}"
 
       within '#student-attendances' do
