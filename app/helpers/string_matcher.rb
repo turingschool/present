ca90643086c26b66c48327a8058ae5083546f7e2
margdaly <margaret.daly@gmail.com>
2024-05-06 14:30:47 -0600

    update: Add pseudo code for #find_jarow_match method to improve readability

--- app/helpers/string_matcher.rb
+++ app/helpers/string_matcher.rb
@@ -7,9 +7,9 @@ module StringMatcher
   end
 
   def find_jarow_match(string, list)
-    jarow = FuzzyStringMatch::JaroWinkler.create(:pure)
-    list.max_by do |list_item|
-      jarow.getDistance(string, list_item)
-    end
+    jarow = FuzzyStringMatch::JaroWinkler.create(:pure) # creates an instance of the Jaro-Winkler algorithm
+    list.max_by do |list_item| # max_by finds the item in the list with the highest similarity to the string
+      jarow.getDistance(string, list_item) # calculates the Jaro-Winkler distance between the input string and the current item in the list
+    end # returns the item in the list with the highest similarity to the input string
   end
 end
