fd0db7a29991f176000fbc06c3d33e673b490cbe
margdaly <margaret.daly@gmail.com>
2024-05-09 10:03:55 -0600

    test: Add #name_distance test to StringMatcher spec

--- spec/helpers/string_matcher_spec.rb
+++ spec/helpers/string_matcher_spec.rb
@@ -6,17 +6,29 @@ RSpec.describe StringMatcher do
       expect(string_distance('John', 'John')).to eq(1.0)
       expect(string_distance('John', 'Jon')).to be_within(0.01).of(0.94)
       expect(string_distance('John J', 'John Johnson')).to be_within(0.01).of(0.91)
-      expect(string_distance('Jon', 'Johnathan')).to be_within(0.01).of(0.82)
       expect(string_distance('John', 'Jane')).to be_within(0.01).of(0.67)
+    end
+
+    it 'returns the correct distance in similarity regardless of case' do
+      expect(string_distance('john', 'John')).to eq(1.0)
+      expect(string_distance('JOHN', 'jon')).to be_within(0.01).of(0.94)
+      expect(string_distance('joHn j', 'John Johnson')).to be_within(0.01).of(0.91)
+    end
+  end
+
+  describe '#name_distance' do
+    it 'returns the correct distance in similarity between two names' do
+      
     end
   end
 
   describe '#find_jarow_match' do
     it 'returns the item in the list with the highest similarity to the input string' do
       list = ['John', 'Jon', 'John Johnson', 'Johnathan', 'Jane']
+
       expect(find_jarow_match('Johnny', list)).to eq('John')
       expect(find_jarow_match('John John', list)).to eq('John Johnson')
-      expect(find_jarow_match('Jack', list)).to eq('Jane')
+      expect(find_jarow_match('Jack J', list)).to eq('Jane')
       expect(find_jarow_match('Bobby', list)).to eq('Jon')
     end
   end
