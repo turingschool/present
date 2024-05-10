fae05713ac1015497a4145fd4cb60207ae53b099
margdaly <margaret.daly@gmail.com>
2024-05-10 13:11:39 -0600

    chore: Update StringMatcher spec with new tests for #find_jarow_match

--- spec/helpers/string_matcher_spec.rb
+++ spec/helpers/string_matcher_spec.rb
@@ -23,6 +23,31 @@ RSpec.describe StringMatcher do
   end
 
   describe '#find_jarow_match' do
+    # used in PopuliFacade to match the turing module name to the course abbrv from Populi API
+    let(:courses) { [
+      'BE Mod 0 Classic',
+      'BE Mod 0 Intensive',
+      'BE Mod 1 Object Oriented Programming with Ruby',
+      'Be Mod 2 Web Application Development',
+      'BE Mod 3 Professional Rails Applications',
+      'BE Mod 4 Cross-Team Processes and Applications',
+      'C#.NET Mod 0',
+      'FE Mod 0 Intensive',
+      'FE Mod 0 Classic',
+      'FE Mod 1 Fundamental Web Technologies',
+      'FE Mod 2 Web Development with JavaScript',
+      'FE Mod 3 Professional Client Side Development',
+      'FE Mod 4 Cross-Team Processes and Applications'
+    ] }
+
+    context 'when the string is exactly one of the list items' do
+      it 'returns the exact match' do
+        expect(find_jarow_match('BE Mod 0 Classic', courses)).to eq('BE Mod 0 Classic')
+        expect(find_jarow_match('FE Mod 0 Intensive', courses)).to eq('FE Mod 0 Intensive')
+      end
+    end
+
+    context 'when the string is similar to one of the list items' do
     it 'returns the item in the list with the highest similarity to the input string' do
       list = ['John', 'Jon', 'John Johnson', 'Johnathan', 'Jane']
 
