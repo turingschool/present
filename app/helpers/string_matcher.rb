b60ad9dca3097cdaae16acec65589750bc637569
margdaly <margaret.daly@gmail.com>
2024-05-13 10:43:52 -0600

    feat: Add #name_distance and text gem to StringMatcher

--- app/helpers/string_matcher.rb
+++ app/helpers/string_matcher.rb
@@ -1,4 +1,5 @@
 require 'fuzzystringmatch'
+require 'text'
 
 module StringMatcher
   def sanitize_name(name)
@@ -9,9 +10,13 @@ module StringMatcher
     end
   end
 
-  def string_distance(s1, s2) # higher number --> more similar
+  def string_distance(s1, s2)
     jarow = FuzzyStringMatch::JaroWinkler.create(:pure)
     jarow.getDistance(s1.downcase, s2.downcase)
+  end
+
+  def name_distance(s_name, s_alias)
+    Text::Levenshtein.distance(sanitize_name(s_name), sanitize_name(s_alias))
   end
 
   def find_jarow_match(string, list)
@@ -19,14 +24,5 @@ module StringMatcher
     list.max_by do |list_item| # max_by finds the item in the list with the highest similarity to the string
       jarow.getDistance(string.downcase, list_item.downcase) # calculates the Jaro-Winkler distance between the input string and the current item in the list
     end # returns the item in the list with the highest similarity to the input string
-  end
-
-  def find_best_match(s1, s2)
-    first_s1, last_s1 = s1.split(' ', 2)
-    first_s2, last_s2 = handle.split(' ', 2)
-    jarow = FuzzyStringMatch::JaroWinkler.create(:pure)
-    first_name_distance = jarow.getDistance(first_s1.downcase, first_s2.downcase)
-    last_name_distance = jarow.getDistance(last_s1[0].downcase, last_s2[0].downcase)
-    (first_name_distance + last_name_distance) / 2.0
   end
 end
