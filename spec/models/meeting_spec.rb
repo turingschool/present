ca225e71c4c4a9df8d535bcd9a95ef2132411d1f
Alejandro Lopez <alejandrolopez1592@gmail.com>
2024-06-03 09:08:40 -0600

    wip: Prepare testing for the new logic for attendance status

---
+++ spec/models/meeting_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Meeting, type: :model do
+  
+
+
+  describe 'instance methods' do
+
+    describe "#record_student_attendance" do
+        
+    end
+  end
+end
--- spec/models/zoom_meeting_spec.rb
+++ spec/models/zoom_meeting_spec.rb
@@ -12,7 +12,82 @@ RSpec.describe ZoomMeeting do
   end
 
   describe "instance methods" do
-    describe "#record_student_attendance_hours"
+    describe "#take_participant_attendance" do
+      before :each do
+        @user = mock_login
+        @turing_module = create(:turing_module)
+        allow(ZoomService).to receive(:access_token)
+        allow(ZoomService).to receive(:meeting_details)
+        @meeting_id = "96851574864"
+        @zoom_meeting = ZoomMeeting.new(meeting_id: @meeting_id)
+        start_time = "2024-05-29T15:00:00Z".to_datetime
+        attributes = {
+          start_time: start_time, 
+          end_time: start_time + 120.minutes, 
+          title: "- Modules ",
+          duration: (120)
+        }
+        @zoom_meeting.update(attributes)
+        @zoom_meeting.turing_module = @turing_module
+        @attendance = Attendance.create(attendance_time: start_time, end_time: start_time + 120.minutes, meeting: @zoom_meeting, turing_module: @turing_module, user: @user)
+        @zoom_meeting.attendance = @attendance
+        stub_request(:get, "https://api.zoom.us/v2/report/meetings/#{@meeting_id}/participants?page_size=300") \
+          .to_return(body: File.read('spec/fixtures/zoom/participant_report_for_student_present_less_than_thirty.json'))
+
+        end
+        
+      context 'when a student misses more than 30 minutes of a class' do
+        it 'records the student as absent' do
+          student_1 = Student.create(name: "Sid Swaminathan", turing_module: @turing_module)
+          student_2 = Student.create(name: "Lito Croy", turing_module: @turing_module)
+          student_3 = Student.create(name: "Karl Fallenius", turing_module: @turing_module)
+          student_4 = Student.create(name: "Cameron Pittman", turing_module: @turing_module)
+          student_5 = Student.create(name: "Tyler Noble", turing_module: @turing_module)
+          
+          @report = ZoomService.participant_report(@meeting_id)[:participants]
+          
+          aliases = @report.map do |participant|
+            {
+              name: participant[:name], 
+              zoom_meeting_id: @zoom_meeting.id, 
+              turing_module_id: @turing_module.id
+            }
+          end
+          ZoomAlias.insert_all(aliases, unique_by: [:name, :turing_module_id])
+
+          ZoomAlias.find_by(name: "sid swaminathan").update(student: student_1)
+          ZoomAlias.find_by(name: "Lito Croy (he/him) BE").update(student: student_2)
+          ZoomAlias.find_by(name: "Karl F (He/Him), BE").update(student: student_3)
+          ZoomAlias.find_by(name: "Cameron P BE").update(student: student_4)
+          ZoomAlias.find_by(name: "Tyler Noble").update(student: student_5)
+
+          grouped_participants = @report.map do |participant| 
+            ZoomParticipant.new(participant, @attendance.attendance_time, @attendance.end_time)
+          end.group_by(&:name)
+
+          @turing_module.students.each do |student|
+            matching_participants = student.zoom_aliases.pluck(:name).flat_map do |zoom_name|
+              grouped_participants[zoom_name]
+            end.compact
+            require 'pry'; binding.pry
+            total_duration = calculate_duration(matching_participants)
+
+            student_attendance = record_student_attendance(student, matching_participants, total_duration)
+            
+            record_student_attendance_hours(matching_participants, student_attendance)
+          end
+
+
+          #expect attendance.student_attendance_hour to include 1 student with status absent named SID
+          #expect attendance.student_attendance_hour to include 3 students that are present
+          #expect attendance.student_attendance_hour to include 1 students that are tardy
+        end
+      end
+        
+
+      describe "#record_student_attendance_hours" do
+      end
+    end
   end
 
   describe 'class methods' do
